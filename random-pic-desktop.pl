acdfab24665fdcd9c614b4e658315cab32226598
eexpress <eexpress@163.com>
2010-02-04 19:09:02 +0800

    整理

--- random-pic-desktop.pl
+++ random-pic-desktop.pl
@@ -2,11 +2,11 @@
 
 use Cwd qw(abs_path);
 # 无图片参数时，使用缺省的桌面文件（链接）●
-my $desk=abs_path($ARGV[0]?$ARGV[0]:"$ENV{HOME}/.desktop-pic");
+my $desk=abs_path($ARGV[0]?$ARGV[0]:"$ENV{HOME}/bin/default.pic");
 
 $picpath='/home/exp/媒体/';	#图片的目录●
 chdir $picpath;
-$num=int 4+rand(5);	#随机取得4-9张照片。png格式。
+$num=int 4+rand(2);	#随机取得4-8张照片。png格式。
 print "total\t-> $num\n";
 my @files = glob "*.png *.jpg";
 unlink glob "/tmp/d-*.png";
@@ -41,12 +41,17 @@ my @files = glob "d-*.png";
 my $cmd="habak ".$desk;
 foreach(@files){
 my $x=int 300+rand(1280-500);		# 屏幕位置范围
-my $y=int rand(800-200);
+my $y=int rand(800-300);
 $cmd=$cmd." -mp $x,$y $_";
 }
-use POSIX qw(strftime);
-my $d=strftime("%Y-%m-%d %A",localtime(time));
-$cmd=$cmd." -mf \"/usr/share/fonts/truetype/arphic/ukai.ttf\" -mc 255,255,255,100 -mh 24 -mp 20,700 -ht \"$d\"";
-#$cmd=$cmd." -mf \"/home/exp/安装/备份/字体/中文字体/经典繁勘亭.ttf\" -mc 255,255,255,100 -mh 24 -mp 20,700 -ht \"$d\"";
+#use POSIX qw(strftime);
+#my $d=strftime("%A",localtime(time));
+#my $d=strftime("%A %Y年 %m月 %d日",localtime(time));
+#$ttf='/home/exp/安装/备份/字体/中文字体/华康娃娃体-WaWaW5.ttf';
+#$ttf='/home/exp/安装/备份/字体/中文字体/方正粗宋简体.ttf';
+#$ttf='/usr/share/fonts/truetype/arphic/ukai.ttf';
+#$cmd=$cmd." -mf \"$ttf\" -mc 255,255,255,100 -mh 48 -mp 20,600 -ht \"$d\"";
+`$ENV{HOME}/bin/cal.pl`;
+$cmd.=" -mp 30,540 /tmp/pango.png";
 print "cmd\t-> $cmd\n";
 `$cmd`;
