4cd9d3747239466aafc791f6353ce4b5c043ec8d
eexpress <eexpress@163.com>
2010-02-19 03:36:02 +0800

    搞定map的写法

---
+++ monitor-clip.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+$_=`xsel -o`;
+print "$_\n";
+if(/http:\/\/[^\s]*/){
+	print "url=$&\n";
+	`opera \"$&\"`;
+	exit;
+	}
+#if(/\@(.*\.com)/){
+if(/\@(.*\.(com|net|org))/){
+	print "domain=$1\n";
+	@l=`nslookup $1`; 
+	$n=0;foreach (@l){$n++; last if /^Name/};
+	$_=@l[$n];
+	}
+if(/\d+\.\d+\.\d+\.\d+/){
+	print "ip=$&\n";
+	`$ENV{HOME}/bin/ip-查询/ip-ip纯真库.pl -n $&`;
+	exit;
+	}
+if(/pps:\/\/[^\s]*/){
+	print "pps=$&\n";
+	`totem \"$&\"`;
+	exit;
+	}
+if(/^\w+$/){
+	print "sdcv now\n";
+	`$ENV{HOME}/bin/bot/sdcv.pl -n`;
+	exit;
+}
+#default
+#translate between en & zh_CN
+`$ENV{HOME}/bin/bot/g-translate.pl -n \"$&\"`;
+
