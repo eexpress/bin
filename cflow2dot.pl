837dec623a62111fbb23d6350d93c3cbe3af94c9
eexpress <eexpress@163.com>
2010-10-21 15:32:19 +0800

    cflow修改

--- cflow2dot.pl
+++ cflow2dot.pl
@@ -2,44 +2,32 @@
 
 if(! -x "/usr/bin/cflow"){print "\'cflow\' not installed.\n"; exit;}
 $in=join " ",@ARGV;
-my @index;
-my @output;
 my @color=qw(#eecc80 #ccee80 #80ccee #eecc80 #80eecc);
 my @shape=qw(box ellipse octagon hexagon diamond);
-my $tmpdot="/tmp/cflow.dot";
+my $pref="/tmp/cflow";
 my $ext="svg";
-my $tmppng="/tmp/cflow.$ext";
 
-foreach (`/usr/bin/cflow -d 2 $in`){
+foreach (`/usr/bin/cflow -l $in`){
 	chomp;
-	s/^ *//; my $n=length($&)/4;
-	my $isnode=m/:$/?1:0;
-	s/\(.*$//;
-	$index[$n]=$_ if($isnode);
-#        print "n=$n\tisnode=$isnode\t$index[$n-1]->$_;\n";
+	s/\(.*$//; s/^\{\s*//; s/\}\s*/\t/;
+	my($n,$f)=split /\t/,$_;
+	$index[$n]=$f;
 	if($n){
-	push @output,"node [color=\"$color[$n-1]\" shape=$shape[$n]];edge [color=\"$color[$n-1]\"];\n";
-	push @output,"$index[$n-1]->$_;\n";}
-	else{push @output,"$_ [shape=box];\n";}
+	$_="$index[$n-1]->$f";
+	push @output,"node [color=\"$color[$n-1]\" shape=$shape[$n]];edge [color=\"$color[$n-1]\"];\n$_\n" if(! $count{$_}++);
+	}
+	else{push @output,"$f [shape=box];\n";}
 }
-unshift @output,"node [peripheries=2 style=\"filled,rounded\" fontname=\"Vera Sans YuanTi Mono\" color=\"$color[0]\"];\nrankdir=LR;\nlabel=\"$in\"\n";
-unshift @output,"\n";
-unshift @output,"digraph G {\n";
+#print @output; exit;
+unshift @output,"digraph G {\nnode [peripheries=2 style=\"filled,rounded\" fontname=\"Vera Sans YuanTi Mono\" color=\"$color[0]\"];\nrankdir=LR;\nlabel=\"$in\"\n";
 push @output,"}\n";
-
-print "dot output to $tmpdot.\n";
-#my @unique=grep {! $count{$_}++ || ! /->/ } @output;
-#$count = 1;
-#for (@unique) {
-#if( /^node/ && $unique[$count]=~/^node/){$_="";}
-#$count++;
-#}
-@unique=@output;
-open FILE,'>',$tmpdot; print FILE @unique;close FILE;
-
+open FILE,'>',"$pref.dot"; print FILE @output;close FILE;
+print "dot output to $pref.dot.\n";
+open (STDERR, ">/dev/null");
 if(-x "/usr/bin/dot"){
-`dot -T$ext $tmpdot -o $tmppng`;
-print "$ext output to $tmppng.\n";
-if(-x "/usr/bin/eog"){`eog $tmppng`;}
+`dot -T$ext "$pref.dot" -o $pref.$ext`;
+print "$ext output to $pref.$ext.\n";
+if(-x "/usr/bin/eog"){`eog $pref.$ext`;}
 }
 else{print "\'dot(graphviz)\' not installed.\n"}
+close STDERR;
