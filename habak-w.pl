21cf5b92c58a19526402768bcff86f542a16873d
eexpress <eexpress@163.com>
2010-10-29 10:09:56 +0800

    habak-w修改

--- habak-w.pl
+++ habak-w.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl
 
-use Encode qw(_utf8_on _utf8_off);
+use Encode qw(_utf8_on _utf8_off encode);
 
 $logf="$ENV{HOME}/bin/resources/weather.log";
-$x0=380;$y0=80;			# 屏幕起始坐标
+$x0=420;$y0=80;			# 屏幕起始坐标
 $icondir="$ENV{HOME}/bin/resources/weather-icon-64";
-$font="~/.fonts/字体/中文字体/YuanTi/VeraSansYuanTi-Bold.ttf";
-$bgfile="~/.fvwm/desktop.jpg";
+$font="$ENV{HOME}/bin/resources/VeraSansYuanTi-Bold.ttf";
+$bgfile="$ENV{HOME}/bin/resources/desktop.jpg";
 $max=7;	#从今天算起，最多显示几天。
 %indexcolor=(
 	">"=>"200,200,200,250",	# 今天
@@ -14,7 +14,31 @@ $max=7;	#从今天算起，最多显示几天。
 	" "=>"200,200,200,150",	# 其他
 );
 # ------以上为可自定义的部分------
-`$ENV{HOME}/bin/weather.pl`;
+#`$ENV{HOME}/bin/weather.pl`;
+@t=localtime((stat($logf))[9]);$fday=($t[5]+1900)."-".($t[4]+1)."-".$t[3];
+@t=localtime(time);$today=($t[5]+1900)."-".($t[4]+1)."-".$t[3];
+#---------------------------------
+if($today ne $fday){	#不是当天的数据
+until($_[0]=~/^Server:/){@_=`nslookup -timeout=1 -retry=1 www.163.com`;}
+use LWP::Simple; $_=get("http://qq.ip138.com/weather/hunan/ChangSha.wml");
+if($_){	#取得了网页。解析。
+	$_=encode("utf8",$_);
+	s/.*?(?=\d{4}-\d)//s;s/\n.*//s;	#去掉头尾无用信息。
+	s/<br\/><br\/><b>/\n/g; s/<br\/>/\t/g; s/<\/b>//g; s/<b>//g;
+	s/℃/°C/g; s/～/-/g; s/\x0d/\n/g;
+	@_=split "\n",$_;
+#---------------------------------
+	use Date::Parse qw/str2time/;
+	open REC,">$logf"; for (@_){
+	if (/$today/){$_=">\t$_";}
+	else{	($day,@t)=split "\t";
+		@t=localtime str2time($day);	# 检查下星期
+		if(($t[6]==0)||($t[6]==6)){$_="-\t$_";}	#周六周日
+		else {$_=" \t$_";}}
+	$_.="\n"; print REC; 
+	}; close REC; }}
+#---------------------------------
+-f $logf || die "can not fetch log file.\n";
 chdir $icondir;
 my $size=`identify -format "%w" 00.png`;  # 以00.png的宽度定基本尺寸
 my $bgs=($size*2)."x".($size*4);
@@ -66,10 +90,7 @@ $x2=$x0+2; $y2=$y1+2; $cmd.="-mc 20,20,20,200 -mp $x2,$y2 -ht $_ ";
 $cmd.="-mc $color -mp $x0,$y1 -ht $_ ";
 $y1+=$h0;
 $fsize=$size/8;
-#$_=decode("utf8",$wind);s/.{10}/$&\n/g;$wind=encode("utf8",$_);
 _utf8_on($wind);$wind=~s/.{10}/$&\n\n/g;_utf8_off($wind);
-#$wind=~s'/'\n'g;
-#$wind=~s'/.*'';
 $cmd.="-mp $x0,$y1 -mh $fsize -ht \"$wind\" ";
 $x0+=$w0;
 }
--- 截图或贴图.bash
+++ 截图或贴图.bash
@@ -8,6 +8,7 @@ f=`date '+%Y-%m-%d-%H:%M:%S'`.png
 s="any"		#注释掉，就使用import；否则使用scrot
 [ -z $s ] && import -frame $f || scrot -sb $f
 convert $f -scale "800x600>" $f
-if [ ! -z $1 ]; then ~/bin/paste-img.pl $f; rm $f;
+#if [ ! -z $1 ]; then ~/bin/paste-img.pl $f; rm $f;
+if [ ! -z $1 ]; then /home/exp/应用/paste-img/pi.pl -s kimag.es $f; rm $f;
 aplay '/home/exp/媒体/事件声音-et/select.wav'; fi
 
