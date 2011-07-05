0dad6deeac3365c933bba7822ee0ed82013d923c
eexpress <eexpress@163.com>
2011-07-05 23:17:43 +0800

    stranger double link & png mime?

--- config/ln-全部隐藏文件到家目录.bash
+++ config/ln-全部隐藏文件到家目录.bash
@@ -2,9 +2,11 @@
 
 d=`pwd`
 for i in `ls -A|grep '^\.'`; do
+#echo "--------------$i"
 #ln -sf $i
 l=`echo $i|sed 's/+/\//g'`
 cmd="ln -sf $d/$i ~/$l"
 echo $cmd
 eval $cmd
 done
+find . -maxdepth 2 -type l -exec rm {} \;
