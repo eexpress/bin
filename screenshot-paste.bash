3ea8d09bfd6f71937b50240dd120cb955791e939
eexpress <eexpress@163.com>
2011-07-06 16:00:10 +0800

    use scrot

--- screenshot-paste.bash
+++ screenshot-paste.bash
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 cd ~/下载
-#play "~/bin/resources/sound/kongas.ogg"
-#f=`date '+%Y-%m-%d-%H:%M:%S'`.jpg
+aplay ~/bin/resources/sound/weapon_pkup.wav
 f=`date '+%Y-%m-%d-%H:%M:%S'`.png
 
-#s="any"		#注释掉，就使用import；否则使用scrot
+s="any"		#注释掉，就使用import；否则使用scrot
 [ -z $s ] && import -frame $f || scrot -sb $f
 convert $f -scale "800x600>" $f
-if [ ! -z $f ]; then ~/bin/paste-img/pasteimg.pl $f; rm $f;
-play "~/bin/resources/sound/kongas.ogg"; fi
+if [ ! -z $f ]; then pasteimg.pl $f; rm $f; fi
 
