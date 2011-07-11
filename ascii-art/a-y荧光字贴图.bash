39a47269e2f329c0dfe190f645099ae6216736b6
eexpress <eexpress@163.com>
2011-07-11 21:20:00 +0800

    pa png

--- ascii-art/a-y荧光字贴图.bash
+++ ascii-art/a-y荧光字贴图.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-convert -fill dodgerblue -background none -font /home/exp/安装/备份/字体/中文字体/经典繁颜体.ttf -pointsize 30 label:"$*" -bordercolor black -border 16x16 \( +clone -blur 0x25 -level 0%,50% \) -compose screen -composite ~/下载/f-荧光字.png
-paste-img.pl ~/下载/f-荧光字.png
+convert -fill dodgerblue -background none -font /home/eexp/.fonts/园体/VeraSansYuanTi-Regular.ttf -pointsize 30 label:"$*" -bordercolor black -border 16x16 \( +clone -blur 0x25 -level 0%,50% \) -compose screen -composite ~/下载/f-荧光字.png
+pasteimg.pl ~/下载/f-荧光字.png
 #rm ~/下载/f-荧光字.png
 
