5e0bd0318308e3b2ceea1d55af333c6c4f671812
eexpress <eexpress@163.com>
2011-08-02 22:38:16 +0800

    ready merge monitor-clip to clip.pl

--- monitor-clip.pl
+++ monitor-clip.pl
@@ -1,56 +1,31 @@
 #!/usr/bin/perl
 
 $_=`xsel -o`;
-s/\s*$//;
-chomp;
-print "$_\n";
-if(/^\// || /^~\//){
-	s/^~/$ENV{HOME}/;
-	if(-f){`xdg-open $_`;}
-	if(-d){`rox $_`;}
-	exit;
+#----------------------------------
+if(/^\// || /^~\//){s/^~/$ENV{HOME}/;if(-e){`xdg-open $_`;exit;}}
+if(/\d+\.\d+\.\d+\.\d+/){`$ENV{HOME}/bin/ip-138查询ip属地.bash $&`;exit;}
+if(/^\w+$/){`$ENV{HOME}/bin/bot/sdcv.pl -n`; exit;}
+if(! /:\/\//){`$ENV{HOME}/bin/bot/g-translate.pl -n \"$&\"`;exit;}
+#----------------------------------
+@url=m"(?:http|mms|rtsp)://[^\s]*"g;
+if($#url<0){print "none url\n";return 1;}
+$TERM='xterm -e';
+foreach(@url){
+my $t=""; s/\W+$//;
+#----------------------------------
+if(/v.youku.com/ || /tudou.com\/playlist/ || /v.ku6.com/ || /6.cn\/watch/        || /tv.sohu.com/){
+   $t="下载flash资源"; `$TERM ~/bin/flash-down.pl $_`;}
+elsif(m"http://u.115.com/file/\w+"){
+   $t="下载115资源"; `$TERM ~/bin/115_client $&`;}  
+elsif(/\.rapidshare.com/ || /www\.hotfile\.com/ || /\.filesonic\.com/){
+   $t="保存slimrat资源"; `$TERM /usr/bin/slimrat $_`;}  
+elsif(/^mms/ || /^rtsp/ || /\.asx$/){
+   $t="播放网络流媒体"; `$TERM mplayer $_`;}  
+#----------------------------------
+if($t){
+	print "$t ===> $_\n"; $t=decode("utf8",$t);
+	`$ENV{HOME}/bin/msg elvis.png  $t $_`;}
+else {print "unrecognized url:\t=>$_<=\n"; return 1;}
 }
-if(m"http://u.115.com/file/\w+"){
-	`$ENV{HOME}/bin/msg elvis.png  "下载115资源" "$&"`;
-	`~/bin/xterm4.pl -e ~/bin/115_down -l \'eexp,115eexp00\' $&`;
-	exit;
-}
-if(/http:\/\/rapidshare\.com\/files\// || /hotfile\.com/ || /\.share-online\.biz/ || /megaupload\.com/ || /www\.fileserve\.com/ || /www\.filesonic\.com/){
-	print "add to queue file.\n";
-
-	`$ENV{HOME}/bin/msg elvis.png  "保存并启动slimrat" "ok"`;
-	`xsel -o >>~/.slimrat/url`; `echo '\n' >>~/.slimrat/url`;
-	exit;
-}
-if(m"http://[^\s]*"){
-	print "url=$&\n";
-	`opera \"$&\"`;
-	exit;
-	}
-#if(/\@(.*\.com)/){
-if(/\@(.*\.(com|net|org))/){
-	print "domain=$1\n";
-	@l=`nslookup $1`; 
-	$n=0;foreach (@l){$n++; last if /^Name/};
-	$_=@l[$n];
-	}
-if(/\d+\.\d+\.\d+\.\d+/){
-	print "ip=$&\n";
-#        `$ENV{HOME}/bin/ip-查询/ip-ip纯真库.pl -n $&`;
-	`$ENV{HOME}/bin/ip-138查询ip属地.bash $&`;
-	exit;
-	}
-if(/pps:\/\/[^\s]*/){
-	print "pps=$&\n";
-	`totem \"$&\"`;
-	exit;
-	}
-if(/^\w+$/){
-	print "sdcv now\n";
-	`$ENV{HOME}/bin/bot/sdcv.pl -n`;
-	exit;
-}
-#default
-#translate between en & zh_CN
-`$ENV{HOME}/bin/bot/g-translate.pl -n \"$&\"`;
+#----------------------------------
 
