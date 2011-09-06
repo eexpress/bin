ec9344f9ec1eac3e3f28fc788043a83565b88400
eexpress <eexpress@163.com>
2011-09-06 15:02:40 +0800

    synclient.bash

---
+++ synclient.bash
@@ -0,0 +1,27 @@
+# !/bin/bash
+
+#取消滚动边
+synclient VertEdgeScroll=0
+synclient HorizEdgeScroll=0
+#双指滚动
+synclient VertTwoFingerScroll=1
+synclient HorizTwoFingerScroll=1
+#开关
+synclient TouchpadOff=0
+#拖放锁定
+synclient LockedDrags=1
+synclient LockedDragTimeout=5000
+#上角右键菜单，下角中键粘贴和后台打开页面
+synclient RTCornerButton=3
+synclient RBCornerButton=2
+synclient LTCornerButton=3
+synclient LBCornerButton=2
+#多指同时点击，模拟按键
+synclient TapButton1=1
+synclient TapButton2=2
+synclient TapButton3=3
+#连击
+synclient ClickFinger1=1
+synclient ClickFinger2=2
+synclient ClickFinger3=3
+
