6f3ae2dc0e86739a25bc2b53383f7667e010d4df
eexpress <eexpress@163.com>
2011-09-15 15:51:40 +0800

    a-irc彩色.pl+pannel

--- ascii-art/a-irc彩色.pl
+++ ascii-art/a-irc彩色.pl
@@ -3,14 +3,18 @@
 use Encode qw(_utf8_on _utf8_off);
 
 srand();
+if($ARGV[1]){
 $_=join ' ',@ARGV;
+}else{
+$_=`xsel -o`;
+}
 _utf8_on($_);
 @_=split //,$_;
 foreach(@_){
 $r=int rand(15);
 #print "\.$_";
 _utf8_off($_);
-print "\x03$r$_";
+$out.=sprintf "\x03$r$_";
 }
-print "\n";
-
+print "$out\n";
+`echo $out|xsel -i`;
