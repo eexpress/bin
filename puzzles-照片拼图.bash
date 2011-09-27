4b80963f0eeb53f917e5cd11e8a939779b17b90a
eexpress <eexpress@163.com>
2011-09-27 21:00:32 +0800

    拼图

--- puzzles-照片拼图.bash
+++ puzzles-照片拼图.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd `dirname $1`
-f=`exif -m -t 0x9003 $1`
+cd `dirname "$1"`
+f=`exif -m -t 0x9003 "$1"`
 echo $f|grep '^20'
 if [ $? -eq 1 ]; then
 f="noexif-"`date '+%Y:%m:%d-%H:%M:%S'`
@@ -9,7 +9,7 @@ else
 f=`echo $f|sed 's/\ /-/g'`
 fi
 
-s=`identify -format "%wx%h" $1`
+s=`identify -format "%wx%h" "$1"`
 x=`echo $s|cut -dx -f1`
 y=`echo $s|cut -dx -f2`
 echo -e "$1 ---->\t$f\t$s"
@@ -24,9 +24,9 @@ t=`echo $t|cut -d. -f1`
 echo -e "\e[34m输出：$f\t文件：$#\t缩放宽度：$s\t列数：$t\e[0m"
 
 rm /tmp/4in1*
-convert -scale $s $* /tmp/4in1
+convert -scale $s "$@" /tmp/4in1
 montage -tile $t -geometry +0+0 -background none /tmp/4in1* ./p-$f.jpg
 eog "./p-$f.jpg"
 zenity --question --title=删除 --text="是否 $# 个删除文件"
-[ $? -eq 0 ] && echo "删除。。。" && rm $*
+[ $? -eq 0 ] && echo "删除。。。" && rm "$@"
 
