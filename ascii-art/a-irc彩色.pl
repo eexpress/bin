990f199484ac8b98d11e5d12a0e7cbf4d1a1eaeb
eexpress <eexpress@163.com>
2011-09-29 16:34:37 +0800

    a-forum

--- ascii-art/a-irc彩色.pl
+++ ascii-art/a-irc彩色.pl
@@ -2,6 +2,10 @@
 
 #use Encode qw(_utf8_on _utf8_off);
 use utf8;
+use Getopt::Long;
+GetOptions('forum'=>\$forum);
+
+@c=qw(red blue green grey yellow bisque brown cyan gold khaki maroon lightcoral tomato orange navy seagreen firebrick);
 
 $s='=';
 srand();
@@ -22,15 +26,21 @@ foreach(@_){
 $r=int rand(15);
 #print "\.$_";
 #_utf8_off($_);
-if($r==0){$out.=sprintf "\x030,1$_\x0f";
-}elsif($r==1){$out.=sprintf "\x031,0$_\x0f";
-}else{$out.=sprintf "\x03$r$_";
+if($forum){
+#-----forum color-----
+	$out.="\[color=$c[$r]\]$_\[/color\]";
+}else{
+#-----irc color-----
+	if($r==0){$out.=sprintf "\x030,1$_\x0f";
+	}elsif($r==1){$out.=sprintf "\x031,0$_\x0f";
+	}else{$out.=sprintf "\x03$r$_";
+	}
 }
 #2种花字
 #$out.=int rand(2)?"\xd2\x89":"\xd2\x88";
 }
 #ctrl序列可以用xev看，粗体^B
-$out="\x02$out";
+if($forum){$out="\[b\]$out\[/b\]";}else{$out="\x02$out";}
 #英文倒字
 #use utf8;
 #$out=~y/a-z/ɐqɔpǝɟƃɥᴉḷʞȷɯuodbɹsʇnʌʍxʎz/;
