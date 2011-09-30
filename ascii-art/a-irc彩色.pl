f3920260d311ac9273487aa627b2cf69cf1f52fb
eexpress <eexpress@163.com>
2011-09-30 10:53:00 +0800

    a-irc+down+mess

--- ascii-art/a-irc彩色.pl
+++ ascii-art/a-irc彩色.pl
@@ -3,7 +3,7 @@
 use Encode qw(_utf8_on _utf8_off);
 use utf8;
 use Getopt::Long;
-GetOptions('forum'=>\$forum);
+GetOptions('forum'=>\$forum, 'down'=>\$down, 'mess'=>\$mess);
 
 @c=qw(red blue green grey yellow bisque brown cyan gold khaki maroon lightcoral tomato orange navy seagreen firebrick);
 
@@ -18,11 +18,18 @@ _utf8_on($_);
 s/[()]//g;
 #s/(?=[\x80-\xff]+)/$s/g; s/(?=[\x00-\x7f，。]+)/$s/g;
 #s/(?=[\x80-\xff]+)/$s/g; s/(?=[_A-Za-z0-9]+)/$s/g;
-s/[_A-Za-z0-9]+/$s$&$s/g; s/(，|。|,|\.)/$&$s/g; s/$s\s*$s/$s/g; s/($s.)$s/$1/g; s/^=//; s/=$//;
+s/[_A-Za-z0-9]+/$s$&$s/g; s/(，|。|,|\.|\?|？)/$&$s/g;
+#s/$s\s*$s/$s/g;
+s/($s.)$s/$1/g; s/^=//; s/=$//;
 print "$_\n";
+#英文倒字
+$_=~y/a-z/ɐqɔpǝɟƃɥᴉḷʞȷɯuodbɹsʇnʌʍxʎz/ if $down;
 @_=split /$s/,$_;
 #@_=/.{0,2}/g;
+@_=split //,$_ if(@_==1);
 foreach(@_){
+#2种花字
+$_.=int rand(2)?"\xd2\x89":"\xd2\x88" if $mess;
 $r=int rand(15);
 #print "\.$_";
 #_utf8_off($_);
@@ -36,13 +43,8 @@ if($forum){
 	}else{$out.=sprintf "\x03$r$_";
 	}
 }
-#2种花字
-#$out.=int rand(2)?"\xd2\x89":"\xd2\x88";
 }
 #ctrl序列可以用xev看，粗体^B
 if($forum){$out="\[b\]$out\[/b\]";}else{$out="\x02$out";}
-#英文倒字
-#use utf8;
-#$out=~y/a-z/ɐqɔpǝɟƃɥᴉḷʞȷɯuodbɹsʇnʌʍxʎz/;
 print "$out\n";
 `echo $out|xsel -i`;
