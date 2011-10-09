d3f35da2d4bb4c9d4d8aeb57c2ca3ef11c078ef0
eexpress <eexpress@163.com>
2011-10-09 22:33:23 +0800

    wine+xrandr

--- wine.sh
+++ wine.sh
@@ -1,9 +1,16 @@
 r=`xrandr|grep -o '.*\*'|cut -d' ' -f 4`
 echo $r
+#n=640x480
+n=1024x768
+#n=1152x864
 d=`dirname "$1"`
 echo $d
 cd "$d"
 e=`basename "$1"`
 #wine explorer /desktop=foo,$r "$e"
-wine explorer /desktop=foo,1024x768 "$e"
+xrandr --output default --mode $n --rate 60
+echo change to $n
+#wine "$e"
+wine explorer /desktop=foo,$n "$e"
+xrandr --output default --mode $r
 
