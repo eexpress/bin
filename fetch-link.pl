91698632282d34e5e71472cd4874ca39f68cd91b
eexpress <eexpress@163.com>
2011-11-02 09:55:36 +0800

    fetch-link

---
+++ fetch-link.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use LWP::UserAgent;
+my $url=shift;
+my $ua=new LWP::UserAgent();
+my $re= $ua->get($url);
+die if (!$re->is_success);
+my $html= $re->content;
+
+#得到页面中所有链接
+while($html=~m{<a .*?href=(["'])(.*?)\1.*?>(<.*?/>)*(.*?)</a>}gsi){
+#print "$2\t--->$4\n";
+my $l=$2; my $t=$4;if($l=~/^http/ and $l!~/com\/$|cn\/$/ and $t!~/^</){print "$l\t--->$t\n";}
+}
+
