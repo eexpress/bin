d510b358008e652237c8e4bf96318cab2eb64596
eexpress <eexpress@163.com>
2011-11-17 20:53:36 +0800

    flash去掉wget

--- flash-down.pl
+++ flash-down.pl
@@ -6,6 +6,11 @@ use WWW::Mechanize;
 use Net::DBus;
 binmode STDOUT, ':utf8';
 
+my $black="\e[30m";my $red="\e[31m";my $green="\e[32m";
+my $yellow="\e[33m";my $blue="\e[34m";my $pink="\e[35m";
+my $cyan="\e[36m";my $white="\e[37m";my $normal="\e[0m";
+my $bold="\e[1m";my $reverse="\e[7m";
+
 my $dir="$ENV{HOME}/视频/";
 -d $dir && chdir $dir;
 
@@ -23,35 +28,33 @@ if ($mech->success()) {
 	if(/当前解析视频.*?<font/s){
 	$_=$&; 
 	s/^.*?>//;s/<.*$//s;s/\s*//g;
-#        print; exit;
-	print "\e[31m\e[1m".$_."\e[0m==================\n";
+	print "$red$bold".$_."$normal==================\n";
 	mkdir "$_"; chdir "$_";
 	my $name="$_";
 	my @link=$mech->find_all_links(text_regex => qr/http:\/\/.*[0-9a-fA-F]*/,);
 	my $size=@link;
 	$bus->Notify("flash", 0, "sunny", "$name", "共获取 $size 个地址。", [], { }, -1);
-#        `xtermcontrol --title "flash-down.pl_下载_$name"`;
 	print "\e]2;flash-down.pl_下载_$name\a";
-	print map "=> \e[32m".$_->url()."\e[0m\n",@link;
+	print map "=> $green".$_->url()."$normal\n",@link;
 	open(LINK,">link.log"); print LINK map $_->url()."\n",@link; close LINK;
 	my $cnt=1; my $proc="▭"x$size;
 	foreach(@link){
 		my $add=$_->url();
-		print "\e[31m下载\e[0m => \e[32m$add\e[0m\n";
+		print "$red 下载$normal => $green$add$normal\n";
 		my $file=sprintf "%02d.flv",$cnt;
-#                `wget -c --tries=5 --user-agent='Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.3) Gecko/2008092416 Firefox/3.0.3' $add -O $file`;
-		`wget -c --tries=5 --user-agent='Opera/9.80 (X11; Linux i686; U; en) Presto/2.6.30 Version/10.60' "$add" -O $file`;
-#                `wget -c --user-agent='Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.3) Gecko/2008092416 Firefox/3.0.3' $add -O $file 2>/dev/null`;
-		$_=$?==0?"■":"□";$proc=~s/▭/$_/; 
-		$bus->Notify("flash", 0, ($?==0?"sunny":"error"), "$name", "已经完成下载 $cnt / $size ，wget返回：$? 。\n进度：$proc", [], { }, -1);
-		print "\e[31m\e[1m$name 已经完成下载 $cnt / $size\e[0m ，wget返回：$? 。进度：\e[31m\e[1m$proc\e[0m\n";
+#                `wget -c --tries=5 --user-agent='Opera/9.80 (X11; Linux i686; U; en) Presto/2.6.30 Version/10.60' "$add" -O $file`;
+		$mech->get($add);
+		$mech->save_content($file);
+		$_=$mech->success()?"■":"□";$proc=~s/▭/$_/;
+#                $_=$?==0?"■":"□";$proc=~s/▭/$_/; 
+		$bus->Notify("flash", 0, ($?==0?"sunny":"error"), "$name", "已经完成下载 $cnt / $size ，返回：$? 。\n进度：$proc", [], { }, -1);
+		print "$red$bold$name 已经完成下载 $cnt / $size$normal ，返回：$? 。进度：$red$bold$proc$normal\n";
 		print "\e]2;$name $proc\a";
-#                print "\e]2;$name 下载进度 $proc\a";
 		$cnt++;
 	}
 #---------------------	
 	if($ENV{flv2avi}){
-	`paplay "$ENV{HOME}/bin/resources/sound/skill_up.wav"`;
+	`paplay "/usr/share/sounds/ubuntu/stereo/service-login.ogg"`;
 	$name=~s/ /-/g;
 	print "\e]2;$name 使用mencoder压缩中。。。\a";
 	$bus->Notify("flash", 0, "sunny", "flash 视频转换成 avi", "$dir/$name.avi", [], { }, -1);
@@ -61,7 +64,7 @@ if ($mech->success()) {
 	$bus->Notify("flash", 0, "sunny", "$name 已经完成下载", "$dir/$name.avi", [], { }, -1);
 	chdir '..';
 	`echo "$ARGV[0]\t《$_》\t结果：$proc">>flash-down.log`;
-	`paplay "$ENV{HOME}/bin/resources/sound/I-need-ammo.wav"`;
+	`paplay "/usr/share/sounds/ubuntu/stereo/service-login.ogg"`;
 	} else {
 	/提示：.*/; $_=$&; s/<br\/>.*$//; 
 	$bus->Notify("flash", 0, "error", "flash 解析失败", "$_\n:(", [], { }, -1);
