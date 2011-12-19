5f9c89dd98eaeabb02b96df91c79d41af2524c4c
eexpress <eexpress@163.com>
2011-12-19 10:21:19 +0800

    get-host

--- get-host.pl
+++ get-host.pl
@@ -5,11 +5,12 @@ else{
 @hosts=("twitter.com", "youtube.com", "blogspot.com","google.com",'plus.google.com');
 }
 
+$server='8.8.8.8';
 for $h (@hosts){
-$_=`w3m -dump http://pp.hawkwithwind.net/nslookup.php?server=$h&nonapi=true`;
+$_=`nslookup $h $server`;
 print "# $h\n";
-@_=$_=~/[0-9.]+/g;
+@_=$_=~/[0-9.]{7,}+/g;
 	for (@_){
-	print "$_\t$h\n";
+	print "$_\t$h\n" if ! /$server/;
 	}
 }
