51c9a604c5f865e2f9c74b21d1efabf23ba4b2a8
eexpress <eexpress@163.com>
2011-12-22 21:23:20 +0800

    vimcolor2texlistings.pl+1

--- vimcolor2texlistings.pl
+++ vimcolor2texlistings.pl
@@ -1,29 +1,43 @@
 #!/usr/bin/perl
 
-$_=$ARGV[0];
+$rgbfile="/usr/share/X11/rgb.txt";
+$_=$ARGV[0]//"/usr/share/vim/vim72/colors/desert.vim";
 die "not vim color file" if ! /\.vim$/;
-@_=`egrep -i 'background|identifier|comment|key|constant' $_`;
-$_=join "", grep /background/, @_;
-/background=(.*)/;
-print "%$1\n";
-$_=`grep $1 /etc/X11/rgb.txt`;
-print;
+open F,"<$rgbfile"; @rgb=<F>; close F;
 
-%hc=("identifier"=>"identifierstyle","comment"=>"commentstyle",
-"key"=>"keywordstyle,emphstyle", 
-"constant"=>"commentstyle,stringstyle,numberstyle"
+print "% color schema from file: >$_<
+\\usepackage{listings}
+\\lstset{
+\tnumbers=left,
+\tshowstringspaces=false,
+\tbreaklines,%这条命令可以让LaTeX自动将长的代码行换行排版
+\ttabsize=4,
+\tframe=shadowbox,rulecolor=\\color[gray]{0.9},rulesepcolor=\\color[gray]{0.2},
+\t%样式
+\tbackgroundcolor=\\color[HTML]{333333},
+\tbasicstyle=\\ttfamily,
+";
+
+%hc=("identif"=>"identifierstyle","comment"=>"commentstyle",
+"statement"=>"keywordstyle,emphstyle", 
+"constant"=>"stringstyle,numberstyle",
 );
-#print @_;
-for (keys %hc){
-$_=join "", grep /$i/i, @_;
-/guifg=(.*)/;
-print "%$i: ..$_..\t..$1..\n";
-if($1=~/^#/){print $i."style=\color[HTML]{".uc($1)."}\n";}
-else{
-	$_=`grep "\<$1\>" /etc/X11/rgb.txt`;
-	/[0-9\ ]+/; $_=$&; s/\ /,/g;
-	print $i."style=\color[RGB]{".$_."}\n";
-}
-#print $i;
-}
 
+my $s=join '|', keys %hc;
+@_=`egrep -i '$s' $_`;
+
+for $i (keys %hc){
+$_=join "", grep /$i/i, @_;
+/guifg=(.*)/o;
+my $cn=$1;
+my $bf=$i=~/state/?"\\bf":"";
+if($cn=~/^#/){$cn=~s/#//g;$out="=$bf\\color[HTML]{".uc($cn)."},\n";}
+else{
+	$_=join '',grep /\t$cn\n/i, @rgb;
+	s/\t.*//;
+	$_=join ',', split /\s+/,$_;
+	$out="=$bf\\color[RGB]{$_}, %$cn\n";
+}
+foreach (split /,/,$hc{$i}){print $_.$out;}
+}
+print "}\n";
