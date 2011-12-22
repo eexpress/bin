96d83987fa0c00d9c0032818eb9ea7e0d4cb268c
eexpress <eexpress@163.com>
2011-12-22 14:23:28 +0800

    html2tex.pl1

--- html2tex.pl
+++ html2tex.pl
@@ -5,16 +5,20 @@ die "$ARGV[0] is not an html file." if ! m"text/html";
 open RC,"<$ARGV[0]"; @_=<RC>; close RC;
 
 $_=join "", grep /<body/, @_;
-/bgcolor="#(.*?)"/; print "\\colorbox[HTML]{$1}{\\begin{minipage}{0.4\\textwidth}";
+/bgcolor="#(.*?)"/; print "\\colorbox[HTML]{$1}{\\begin{minipage}{0.8\\textwidth}\n";
 /text="#(.*?)"/; $textcolor=uc($1);
 
 for (@_){
-s/&nbsp;//g; s/&quot;/"/g; s/&amp;/\\&/g; s/&gt;/>/g;s/&lt;/</g;
-s/\\/\\\\/g;
-s/[\$\&\%\#\_\^\{\}\~]/\\$&/g;
+next if /<title/;
+s/\\/\\textbackslash /g;
+s/&quot;/"/g; s/&gt;/>/g;s/&lt;/</g;
+s/&nbsp;/\\quad /g; s/&amp;/\\&/g; 
+s/[\$\%\#\_\^\{\}\~]/\\$&/g;
+#s/[\$\&\%\#\_\^\{\}\~]/\\$&/g;
 s|<font color="\\#(.*?)">(.*?)</font>|"\\color[HTML]{".uc($1)."}{$2}\\color[HTML]{$textcolor}"|eg;
 s"<b>(.*?)</b>"\\textbf{$1}"g;
 s/<.*?>//g;
+next if /^$/;
 s/$/\n/g;
 print;
 
