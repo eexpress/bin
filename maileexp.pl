73e714b0b109dc6158af68656c4c31bc22d658f3
eexpress <eexpress@163.com>
2012-01-12 16:57:24 +0800

    maileexp

--- maileexp.pl
+++ maileexp.pl
@@ -3,29 +3,30 @@
 if($#ARGV<0){die "没有选择附件文件。\n"}
 # --------------------------------------------
 $_=`hostname`; chomp;
-$d=/desktop/?1:0;
+$d=/desktop/?1:2;
 my $to="eexp0$d\@gmail.com";
 my $file=join ",",@ARGV;
-my $info="send $to attach with $file\n";
-print "$info"; `$ENV{HOME}/bin/msg mail.png "mailto-附件" "$info"`;
+my $info="发送文件: $file 到邮箱: $to";
+#● echo password|gpg -aer eexp01>~/bin/resources/gpg-163-password
+my $pw=`gpg -d $ENV{HOME}/bin/resources/gpg-163-password`; chomp $pw;
+print "$info\n"; `$ENV{HOME}/bin/msg mail.png "发送邮件附件" "$info"`;
 # --------------------------------------------
 use Mail::Sender;   
 my $sender = new Mail::Sender; 
-#● echo youremailpassword|gpg -aer eexp01>~/bin/resources/gpg-163-password
-my $pw=`gpg -d $ENV{HOME}/bin/resources/gpg-163-password`; chomp $pw;
 if ($sender->MailFile({
 	smtp => 'smtp.163.com',
 	from => 'eexpress@163.com',
 	to =>$to,
 	charset=>'utf-8',
-	subject => '同步文件。发送附件',
+	subject => '同步文件。',
+	charset => 'utf-8',
 	msg => "请查看附件。",
 	auth => 'LOGIN',
 	authid => 'eexpress',
 	authpwd => $pw,
 	file => \@ARGV,
-})<0){$info="文件 $file 发送到 $to 失败\n$Mail::Sender::Error\n";}
-else {$info="文件 $file 已经发送到 $to\n";}
-print "$info"; `$ENV{HOME}/bin/msg mail.png "mailto-附件" "$info"`;
+})<0){$info="失败。错误：$Mail::Sender::Error";}
+else {$info="完成";}
+print "$info\n"; `$ENV{HOME}/bin/msg mail.png "发送邮件附件" "$info"`;
 # --------------------------------------------
 
