092ab09cc195be87c1a676974c537d01d90c919a
eexpress <eexpress@163.com>
2012-04-12 15:03:19 +0800

    2012-04-12 15:03:19

--- ml.sock.pl
+++ ml.sock.pl
@@ -2,7 +2,7 @@
   
 use IO::Socket::INET;  
   
-my $sock = IO::Socket::INET->new(PeerAddr => 'localhost', PeerPort => 4000) or die "can not create client.\n";  
+my $sock = IO::Socket::INET->new(PeerAddr => 'localhost', PeerPort => 4000) or die "没找到mlnet的端口。\n";  
 
 print "--命令: vd bw_toggle help kill etc.-->";
 while(<>){
