45d38aebe23f9f4071b2b4aca3c426b9547bf256
eexpress <eexpress@163.com>
2012-05-07 16:58:22 +0800

    ip查询只能get，不能post了。

--- ip-138查询ip属地.bash
+++ ip-138查询ip属地.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-r=`echo -n "ip="$1"&action=2"|w3m -dump -no-cookie http://www.ip138.com/ips8.asp -post -|grep '数据：'|sed 's/.*：//'`
+#r=`echo -n "ip="$1"&action=2"|w3m -dump -no-cookie http://www.ip138.com/ips8.asp -post -|grep '数据：'|sed 's/.*：//'`
+r=`w3m -dump -no-cookie http://www.ip138.com/ips138.asp?ip=$1|grep '数据：'|sed 's/.*：//'`
+
 echo $r
 ~/bin/msg "IP地址查询：$1" $r
