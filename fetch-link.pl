68de8c1241255633d40531bd6b2d0359ef95d006
eexpress <eexpress@163.com>
2012-05-17 19:47:58 +0800

    fetch-link

--- fetch-link.pl
+++ fetch-link.pl
@@ -10,7 +10,7 @@ my $html= $re->content;
 #print $html;
 #得到页面中所有链接
 while($html=~m{<a .*?href=(["'])(.*?)\1.*?>(<.*?/>)*(.*?)</a>}gsi){
-#print "$2\t--->$4\n";
-my $l=$2; my $t=$4;if($l=~/^http/ and $l!~/com\/$|cn\/$/ and $t!~/^</){print "$l\t--->$t\n";}
+print "$2\t--->$4\n";
+#my $l=$2; my $t=$4;if($l=~/^http/ and $l!~/com\/$|cn\/$/ and $t!~/^</){print "$l\t--->$t\n";}
 }
 
