2715b3df40487d824919c13d086824ebe616b573
eexpress <eexpress@163.com>
2012-05-18 16:43:07 +0800

    flow自动流程图

---
+++ flow.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+open IN,"<$ARGV[0]"; @_=<IN>; close IN;
+$base=$ARGV[0];$base=~s/\..*//;
+$ext="svg";
+$sep='///';
+#--------------------------------
+@v=map {s'.*///'';s/^\s*//;s/\s*$//;chomp $_;$_} grep $sep,@_;
+$out="";
+for(@v){
+push @output, "$_".'[peripheries=2 shape=ellipse];'."\n" if ! $out;
+$out.="$_->",next if ! /\?/;
+@t=split /[?:]/,$_;
+push @output,"$t[0]".'[shape=diamond]';
+push @output,"$out$t[0];\n";
+push @output,"$t[0]:s->$t[1]".'[label="Yes"];'."\n";
+push @output,"$t[0]:e->$t[2]".'[label="No" style=dotted];'."\n";
+$out=" ";
+}
+$out=~s/->$/;\n/;
+push @output,$out;
+for(@output){print $_;}
+#--------------------------------
+unshift @output,'
+digraph G {
+node [shape=box style=filled fontname="Vera Sans YuanTi Mono"]
+';
+push @output,'}\n';
+
+open OUT,">$base.dot"; print OUT @output;close OUT;
+`dot -T$ext "$base.dot" -o $base.$ext`;
+`eog $base.$ext`;
