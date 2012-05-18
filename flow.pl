49eaddcca645b2f9e4b2e4bcb35b95c5f044b178
eexpress <eexpress@163.com>
2012-05-18 23:49:44 +0800

    flow+2

--- flow.pl
+++ flow.pl
@@ -3,29 +3,47 @@
 open IN,"<$ARGV[0]"; @_=<IN>; close IN;
 $base=$ARGV[0];$base=~s/\..*//;
 $ext="svg"; $sep='///'; $font='Vera Sans YuanTi'; $color="#6495ED";
+$next="_xxx_"; $ends='[shape=ellipse];'."\n";
+$out=""; @isdia=[];
 #--------------------------------
 @v=map {s".*\Q$sep\E"";s/^\s*//;s/\s*$//;chomp $_;$_} grep /$sep/,@_;
-$out="";
-for(@v){
-push @output, "$_".'[shape=ellipse];'."\n" if ! $out;
-$out.="$_->",next if ! /\?/;
-@t=split /[?:]/,$_;
-push @output,"$t[0]".'[shape=diamond]';
-push @output,"$out$t[0];\n";
-push @output,"$t[0]:s->$t[1]".'[label="Yes"];'."\n";
-push @output,"$t[0]:e->$t[2]".'[label="No" style=dotted];'."\n";
-$out=" ";
-}
-$out=~s/->$/;\n/;
+for $x (@v){
+	push @output, "\t$x".$ends if ! $out;
+	if ($x!~/\?/){ 
+		for(@output){s/\Q$next\E/$x/;}
+		$out.="$x->";
+		next;
+		}
+	# 条件判断
+	@t=[];@t=split /[?:]/,$x;
+	for(@output){s/\Q$next\E/$t[0]/;}
+	$out=~s/^\s*//,push @output,"$out$t[0];\n" if $out=~/->/;
+	push @output,"\t$t[0]".'[shape=diamond];'."\n"; push @isdia,$t[0];
+
+	$_=$t[1];
+	if(/^>/){s/^>//; push @output,"\t$_".$ends;} #返回，设置形状，不继续节点
+	else{if($_ ne ""){push @output,"$_->$next;\n";}else{$_=$next;}}
+	push @output,"$t[0]:s->$_".'[label="Yes"];'."\n";
+
+	$_=$t[2];
+	if(/^>/){s/^>//; push @output,"\t$_".$ends;} #返回，设置形状，不继续节点
+	else{if($_ ne ""){push @output,"$_->$next;\n";}else{$_=$next;}}
+	push @output,"$t[0]:e->$_".'[label="No" style=dotted];'."\n";
+
+	$out=" ";
+	}
+
+for(@output){ # 判断的入口，全部顶部
+	$x=$_; for(@isdia){$x=~s/->$_/->$_:n/;} $_=$x;}
+
+$out=~s/^\s*//;$out=~s/->$/;\n/;
 push @output,$out;
-for(@output){print $_;}
 #--------------------------------
 unshift @output,"
 digraph G {
 node [peripheries=2 color=\"$color\" shape=box style=filled fontname=$font]
 ";
-push @output,'}\n';
-
+push @output,"}\n";
 open OUT,">$base.dot"; print OUT @output;close OUT;
 `dot -T$ext "$base.dot" -o $base.$ext`;
 `eog $base.$ext`;
