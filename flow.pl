702adc0b4545e6af5d0a719937c2ac2d0bf448c5
eexpress <eexpress@163.com>
2012-05-21 15:46:10 +0800

    flow+7

--- flow.pl
+++ flow.pl
@@ -1,5 +1,18 @@
 #!/usr/bin/perl
 
+if(($#ARGV==-1) || ($ARGV[0]=~/--help|-h/i) || ! -f "$ARGV[0]"){
+print <<HELP;
+自动根据注释里面的///后面的内容，生成流程图。依赖graphviz。
+flow.pl 文件【各类语法的源码，只要注释不和///冲突】
+语法说明：
+	xxx> 表示函数入口。通常是函数名。
+	>xxx 表示函数出口。通常是return。
+	xxx?yyy:zzz 条件判断语句。yyy为真，zzz为假。可省略其一。如：xxx?yyy 或者 xxx?:zzz。
+	循环体，如while if等，写成条件判断的时候，:后面的假分支，如果指向return返回语句，必须也加上>的前缀。
+HELP
+exit 0;
+}
+
 open IN,"<$ARGV[0]"; @_=<IN>; close IN;
 $base=$ARGV[0];$base=~s/\..*//;
 $ext="svg"; $sep='///'; $font='Vera Sans YuanTi'; $color="#6495ED";
@@ -20,7 +33,7 @@ for $i (@v){
 		$i=~s/>//g;
 		if($needreplace){for(@output){s/\Q$next\E/$i/;}};
 		$needreplace=0; $out.="$i->";
-print $out."--------$i---\n";
+#print $out."--------$i---\n";
 		next;
 		}
 	# 条件判断
