3e6fb25f6e61a5f4356726ace58c01c60cad36c2
eexpress <eexpress@163.com>
2012-05-21 21:39:41 +0800

    flow+10

--- flow.pl
+++ flow.pl
@@ -16,7 +16,6 @@ exit 0;
 open IN,"<$ARGV[0]"; @_=<IN>; close IN;
 $base=$ARGV[0];$base=~s/\..*//;
 $ext="svg"; $sep='///'; $font='Vera Sans YuanTi';
-#$color="#6495ED";
 @color=qw(#6495ED #eecc80 #ccee80 #80ccee #eecc80 #80eecc); $cnt=@color; $cc=0;
 $next="_xxx_"; $end=";\n";
 $send='[shape=ellipse]'.$end;
@@ -29,11 +28,12 @@ for $i (@v){
 	if($i=~/>$/){ #入口
 		$out=~s/->$//g; push @output,$out.$end."}\n" if $out=~/->/;
 	push @output, "\nsubgraph{\t".'node [color="'.($color[$cc%$cnt]).'"]'.$end; $cc++;
-		$i=~s/>//g; push @output, "\t".$i.$send;
+		@hasshape=[];
+		$i=~s/>//g; setshape($i,$send);
 		$needreplace=0; $out="$i->"; next;
 		}
 	if ($i!~/\?/){  #常规，包括出口
-		if($i=~/^>/){$i=~s/^>//; push @output,"\t$i".$send;}
+		if($i=~/^>/){$i=~s/^>//; setshape($i,$send);}
 		if($needreplace){for(@output){s/\Q$next\E/$i/;}};
 #        $i=~s/>//g;
 		$needreplace=0; $out.="$i->"; next;
@@ -43,26 +43,14 @@ for $i (@v){
 	if($needreplace){for(@output){s/\Q$next\E/$t[0]/;}};
 	push @output,$out.$t[0].$end if $out=~/->/;
 	$out="";
-	push @output,"\t".$t[0].$dend; push @isdia,$t[0];
+	setshape($t[0],$dend); push @isdia,$t[0];
 
-	$_=$t[1];
-	if(/^>/){s/^>//; push @output,"\t$_".$send;} #返回，设置形状，不继续节点
-	else{if($_ ne ""){push @output,"$_->$next;\n";}else{$_=$next;}}
-	push @output,"$t[0]:s->$_".'[label="Yes"];'."\n";
-
-	$_=$t[2];
-	if(/^>/){s/^>//; push @output,"\t$_".$send;} #返回，设置形状，不继续节点
-	else{if($_ ne ""){push @output,"$_->$next;\n";}else{$_=$next;}}
-	push @output,"$t[0]:e->$_".'[label="No" style=dotted];'."\n";
-
-	$needreplace=1;
-	}
+	jump($t[1],"y"); jump($t[2],"n"); $needreplace=1;
+}
 #--------------------------------
 for(@output){ # 判断的入口，全部顶部
 	$i=$_; for(@isdia){$i=~s/->$_/->$_:n/;} $_=$i;}
-#$out=~s/^\s*//;
 $out=~s/->$//g; push @output,$out.$end if $out=~/->/;
-#push @output,"}\n";
 #--------------------------------
 unshift @output,"
 digraph G {
@@ -72,3 +60,18 @@ push @output,"}\n}\n";
 open OUT,">$base.dot"; print OUT @output;close OUT;
 `dot -T$ext "$base.dot" -o $base.$ext`;
 `eog $base.$ext`;
+#--------------------------------
+sub jump(){
+	my ($_, $YN)=@_;
+	if(/^>/){s/^>//; setshape($_,$send);} #返回，设置形状，不继续节点
+	else{if($_ ne ""){push @output,"$_->$next;\n";}else{$_=$next;}}
+	if($YN eq "y"){push @output,"$t[0]:s->$_".'[label="Yes"];'."\n";}
+	else{push @output,"$t[0]:e->$_".'[label="No" style=dotted];'."\n";}
+}
+#--------------------------------
+sub setshape(){ # 名称，形状
+	my ($name, $shape)=@_;
+	my $has=grep /^\Q$name\E$/,@hasshape;
+	if($has==0){push @hasshape,$name, push @output,"\t".$name.$shape;}
+}
+#--------------------------------
