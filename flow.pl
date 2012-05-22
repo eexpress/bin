647f4f237e7d5c040b63d3170d7c491b71c2cd95
eexpress <eexpress@163.com>
2012-05-22 09:49:22 +0800

    flow+12

--- flow.pl
+++ flow.pl
@@ -1,7 +1,16 @@
 #!/usr/bin/perl
 
+#----------define here-----------
+$ext="svg"; $sep='///'; $font='Vera Sans YuanTi';
+@color=qw(#6495ED #eecc80 #ccee80 #80ccee #eecc80 #80eecc);
+$send='[shape=ellipse]'.$end; $dend='[shape=diamond]'.$end;
+#--------------------------------
 if(($#ARGV==-1) || ($ARGV[0]=~/--help|-h/i) || ! -f "$ARGV[0]"){
 print <<HELP;
+AUTHOR:		eexpress
+VERSION:	1.1
+USAGE:		flow.pl source_file
+DESCRIPTION:
 自动根据注释里面的///后面的内容，生成流程图。依赖graphviz。
 flow.pl 文件【各类语法的源码，只要注释不和///冲突】
 语法说明：
@@ -12,41 +21,33 @@ flow.pl 文件【各类语法的源码，只要注释不和///冲突】
 HELP
 exit 0;
 }
-
+#--------------------------------
+$cnt=@color; $cc=0; $end=";\n"; $out=""; @isdia=[];
 open IN,"<$ARGV[0]"; @_=<IN>; close IN;
 $base=$ARGV[0];$base=~s/\..*//;
-$ext="svg"; $sep='///'; $font='Vera Sans YuanTi';
-@color=qw(#6495ED #eecc80 #ccee80 #80ccee #eecc80 #80eecc); $cnt=@color; $cc=0;
-$next="_xxx_"; $end=";\n";
-$send='[shape=ellipse]'.$end;
-$dend='[shape=diamond]'.$end;
-$out=""; @isdia=[];
 @v=map {s".*\Q$sep\E"";s/^\s*//;s/\s*$//;chomp $_;$_} grep /$sep/,@_;
 #--------------------------------
-for $i (@v){
+for $j (0 .. $#v){
+	$i=$v[$j];
 	next if $i eq "";
 	if($i=~/>$/){ #入口
 		$out=~s/->$//g; push @output,$out.$end if $out=~/->/;
 		push @output,"}\n" if $cc;
-	push @output, "\nsubgraph{\t".'node [color="'.($color[$cc%$cnt]).'"]'.$end; $cc++;
+		push @output, "\nsubgraph{\t".'node [color="'.($color[$cc%$cnt]).'"]'.$end; $cc++;
 		@hasshape=[];
 		$i=~s/>//g; setshape($i,$send);
-		$needreplace=0; $out="$i->"; next;
+		$out="$i->"; next;
 		}
 	if ($i!~/\?/){  #常规，包括出口
 		if($i=~/^>/){$i=~s/^>//; setshape($i,$send);}
-		if($needreplace){for(@output){s/\Q$next\E/$i/;}};
-#        $i=~s/>//g;
-		$needreplace=0; $out.="$i->"; next;
+		$out.="$i->"; next;
 		}
 	# 条件判断，包括出口
 	@t=[];@t=split /[?:]/,$i;
-	if($needreplace){for(@output){s/\Q$next\E/$t[0]/;}};
 	push @output,$out.$t[0].$end if $out=~/->/;
 	$out="";
 	setshape($t[0],$dend); push @isdia,$t[0];
-
-	jump($t[1],"y"); jump($t[2],"n"); $needreplace=1;
+	jump($t[1],"y"); jump($t[2],"n");
 }
 #--------------------------------
 for(@output){ # 判断的入口，全部顶部
@@ -65,7 +66,8 @@ open OUT,">$base.dot"; print OUT @output;close OUT;
 sub jump(){
 	my ($_, $YN)=@_;
 	if(/^>/){s/^>//; setshape($_,$send);} #返回，设置形状，不继续节点
-	else{if($_ ne ""){push @output,"$_->$next;\n";}else{$_=$next;}}
+	else{my $x=$v[$j+1]; $x=~s/\?.*//;
+	if($_ ne ""){push @output,"$_->$x;\n";}else{$_=$x;}}
 	if($YN eq "y"){push @output,"$t[0]:s->$_".'[label="Yes"];'."\n";}
 	else{push @output,"$t[0]:e->$_".'[label="No" style=dotted];'."\n";}
 }
