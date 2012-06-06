47c6ec9780e64a2efbc11cac09517b0a9e107acd
eexpress <eexpress@163.com>
2012-06-06 10:44:21 +0800

    hardln+1

--- find-hardln-here.bash
+++ find-hardln-here.bash
@@ -8,6 +8,5 @@ j=`find . -inum $i`
 [ $j ] && echo "$i $f => $j"
 done
 #find -inum `ls -i1 "$1"|cut -d' ' -f 1`
-#● for i in ~/Ubuntu\ One/opera/ln.config/*; do find-hardln-here.bash "$i"; done
 #● find . -type f -links +1
-
+#● find-hardln-here.bash ~/Ubuntu\ One/opera/ln.config/*
