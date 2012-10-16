59032597b33c03c1e0ded5916694c5521e4c17ef
eexpress <eexpress@163.com>
2012-10-16 18:12:34 +0800

    monitor use web-translate

--- monitor-clip.pl
+++ monitor-clip.pl
@@ -4,19 +4,27 @@ use Encode qw(decode);
 
 $_=`xsel -o`;
 #----------------------------------
+#/和~开头的存在的文件，打开
 if(/^\// || /^~\//){s/^~/$ENV{HOME}/;s/\n.*//;if(-e){`xdg-open \"$_\"`;exit;}}
+#ip格式的数字，查询
 if(/\d+\.\d+\.\d+\.\d+/){`$ENV{HOME}/bin/ip-138.bash $&`;exit;}
+#域名查询
 if(/(\w+\.){2,3}\w+/){`$ENV{HOME}/bin/ip-138.bash $_`;exit;}
+#单词，本地翻译
 if(/^\w+$/){`$ENV{HOME}/bin/bot/sdcv.pl -n`; exit;}
+#ppa源，添加
 if(/ppa:.*\/ppa/){`zenity --question --title="是否添加此PPA源" --text=$&`; `gksudo add-apt-repository $&` if ! $?; exit;}
-if(! /:\/\//){`$ENV{HOME}/bin/bot/g-translate.pl -n \"$&\"`;exit;}
+#其他没://的，网页翻译
+if(! /:\/\//){`$ENV{HOME}/bin/web-translate.pl \"$&\"`;exit;}
 #----------------------------------
+#url处理
 @url=m"(?:http|mms|rtsp)://[^\s]*"g;
 if($#url<0){print "none url\n";return 1;}
 $TERM='xterm -e';
 foreach(@url){
 my $t=""; s/\W+$//;
 #----------------------------------
+#特定网址和流媒体
 if(/v.youku.com/ || /tudou.com\/playlist/ || /v.ku6.com/ || /6.cn\/watch/        || /tv.sohu.com/){
    $t="下载flash资源"; `$TERM ~/bin/flash-down.pl $_`;}
 elsif(m"http://u.115.com/file/\w+"){
