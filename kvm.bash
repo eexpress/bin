323141f9f9e8dbfa7a018641aa866386f0a95f52
eexpress <eexpress@163.com>
2012-11-17 15:49:50 +0800

    kvm.bash

--- kvm.bash
+++ kvm.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-file $1|grep partition
-[ $? != 0 ] && echo 参数1不是img影像文件 && exit
+#file $1|grep partition
+#[ $? != 0 ] && echo 参数1不是img影像文件 && exit
+e=${1##*.}
+[ $e != "img" ] && echo 参数1不是img影像文件 && exit
 
 if [ $2 ]; then
 file $2|grep CD-ROM
