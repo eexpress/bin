b23ac01f84aed0e4c71ded99800b58eb2931304d
eexpress <eexpress@163.com>
2013-01-09 16:28:58 +0800

    rss

--- bot/rss.pl
+++ bot/rss.pl
@@ -1,13 +1,6 @@
 #!/usr/bin/perl
 
-sub cv {
-	open(CV, "|/usr/bin/enconv|ascii2uni -a D -q|tr '\n' ' '") or die("没有命令：enconv。\n");
-	print CV $_[0];
-	close CV;
-}
-#http://forum.^ubuntu.org.cn/feed.php
 @RSS=qw(
-http://^hsyyf.me/feed/
 http://cn.^engadget.com/rss.xml
 http://^linuxtoy.org/feed/
 http://feed.feedsky.com/^ldcn
@@ -21,14 +14,7 @@ http://www.^ibm.com/developerworks/cn/views/rss/customrssatom.jsp?zone_type=AllZ
 
 if(!$ARGV[0]){
 print "如果没有直接指定rss地址。可输入单词，在全部rss地址列表里，按照次序匹配：";
-foreach(@RSS){
-s/http.*\^//g;s/\..*$//g;
-print " ► ".$_;
-}
-#my $_=join(" ►  ",@RSS);
-#s/http:\/\///g;
-#print "全部rss地址列表，按照次序匹配：".$_;
-#print "全部rss地址列表，按照次序匹配：".join(" ►  ",@RSS);
+foreach(@RSS){ s/http.*\^//g;s/\..*$//g; print " ► ".$_; }
 exit;
 }
 
@@ -47,17 +33,13 @@ my $re= $ua->get($url);
 die if (!$re->is_success);
 my $html= $re->content;
 
-$n=8;
 print "RSS新闻：";
 #得到页面中所有RSS标题和链接
 while($html=~m{<title>(.*?)</title>.*?<link>(.*?)</link>}gsi){
-#cv "► $1 --> $2 ";
 $_="► $1 --> $2 ";
 s/&amp;/&/g; s/&gt;/>/g; s/&lt;/</g; s/&quot;/"/g; s/&nbsp;/ /g;
 s/<!\[CDATA\[//g; s/]]>//g; s/&p=[0-9#p]*//g;
-cv $_;
-#print;
-$n--; last if ($n==0);
-if($n==4){print "\n";};
+s/\n//g; s/&.*?;//g;
+print;
 }
 
