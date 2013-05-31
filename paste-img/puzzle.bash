ada4765f34b5a89434c8630b5074bdee1c855a3e
eexpress <eexpress@163.com>
2013-05-31 21:31:59 +0800

    puzzle改成查表

--- paste-img/puzzle.bash
+++ paste-img/puzzle.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#for i in "$@"; do echo \"${i#file://}\" >>~/tmp; done
-#exit
+[ $# -gt 16 ] && echo "select less than 16 pictures." && exit
 cd "`dirname "$1"`"
 f=`exif -m -t 0x9003 "${1#file://}"`
 echo $f|grep '^20'
@@ -11,35 +10,24 @@ else
 f=`echo $f|sed 's/\ /_/g'|sed 's/:/-/g'`
 fi
 
-s=`identify -format "%wx%h" "$1"`
-x=`echo $s|cut -dx -f1`
-y=`echo $s|cut -dx -f2`
-echo -e "$1 ---->\t$f\t$s"
-if [ $x -gt $y ]; then
-p=0.99; s=720
+aw=(0 1 1 1 2 2 2 3 3 3 4 4 4 4 4 4 4)
+ah=(0 1 2 3 2 3 3 4 4 4 4 4 4 5 5 5 4)
+i=`identify -format "%wx%h" "$1"`
+w=`echo $i|cut -dx -f1`
+h=`echo $i|cut -dx -f2`
+if [ $w -gt $h ]; then
+t=${aw[$#]}
+s=666
 else
-p=1.5; s=480
-#if [ $# -lt 5 ]; then p=0; s=600; fi
-fi
-t=`echo "sqrt($#)+$p"|bc -l`
-t=`echo $t|cut -d. -f1`
-#t=`printf %d $t`
-if [ $# -lt 4 ]; then
-if [ $x -gt $y ]; then
-t=1
-else
-t=$#
-fi
+t=${ah[$#]}
+s=400
 fi
 echo -e "\e[34m输出：$f\t文件：$#\t缩放宽度：$s\t列数：$t\e[0m"
 
 l=`basename $0`
 l=${l%%.*}
-rm /tmp/$l*
-convert -scale $s "$@" /tmp/$l
-montage -tile $t -geometry +0+0 -background none /tmp/$l* ./p-$f.jpg
+montage -tile $t -geometry $s -background none "$@" ./p-$f.jpg
 eog "./p-$f.jpg"
-zenity --question --title=删除 --text="是否 $# 个删除文件"
-[ $? -eq 0 ] && echo "删除。。。" && for i in "$@"; do j=${i#file://}; echo $j>>/tmp/$l.log; rm "$j"; done
-#[ $? -eq 0 ] && echo "删除。。。" && rm "$@"
+zenity --question --title=删除 --text="是否删除 $# 个文件并保留拼图结果"
+[ $? -eq 0 ] && echo "删除。。。" && for i in "$@"; do j=${i#file://}; echo $j>>/tmp/$l.log; rm "$j"; done || rm "./p-$f.jpg"
 
