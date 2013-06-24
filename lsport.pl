82cbb6fc18cc3bd0fb8d89d0e9ed6cbc02f34729
eexpress <eexpress@163.com>
2013-06-24 22:50:22 +0800

    lsport.pl

--- lsport.pl
+++ lsport.pl
@@ -1,17 +1,20 @@
 #!/usr/bin/perl
 
 $_=$ARGV[0]//"localhost";
-print "端口\t\t状态\t服务\t\t服务名\t   端口名\n";
+print "端口   服务          进程  命令\n";
 @_=`/usr/bin/nmap $_`;
 for (@_){
 chomp;
+s/\/tcp//;
 if (/^\d+/){
 @s=grep /IPv.*LISTEN/, `sudo lsof -i :$&`;
 @n=split /\s+/, $s[0];
-@l=split /\s+/, "$_\t$n[0]\t$n[8]\n";
+@l=split /\s+/;
+$c=`cat /proc/$n[1]/cmdline`;
+$c=~s/\0/\ /g;
 format STDOUT=
-^<<<<<<<<<<<<<<<^<<<<<<<^<<<<<<<<<<<<<<<^<<<<<<<<<<^<<<<<<<<<<<<<<<<<
-$l[0],$l[1],$l[2],$l[3],$l[4]
+^<<<<<<^<<<<<<<<<<<<<^<<<<<^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+$l[0],$l[2],$n[1],$c
 .
 write
 }
