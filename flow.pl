c73cda30cba307fafc2122dacb65b1b76ab1ce04
eexpress <eexpress@163.com>
2013-08-19 14:30:56 +0800

    flow1.4

--- flow.pl
+++ flow.pl
@@ -8,7 +8,7 @@ $send='[shape=ellipse]'.$end; $dend='[shape=diamond]'.$end;
 if(($#ARGV==-1) || ($ARGV[0]=~/--help|-h/i) || ! -f "$ARGV[0]"){
 print <<HELP;
 AUTHOR:		eexpress
-VERSION:	1.3
+VERSION:	1.4
 USAGE:		flow.pl source_file
 DESCRIPTION:
 自动根据注释里面的///后面的内容，生成流程图。依赖graphviz。
@@ -16,7 +16,7 @@ flow.pl 文件【各类语法的源码，只要注释不和///冲突】
 语法说明：
 	xxx> 表示函数入口。通常是函数名。必须有一个。
 	>xxx 表示函数出口或者跳转。通常是return或者else/break/next/continue等强制分支。
-	xxx?yyy:zzz 条件判断语句。yyy为真，zzz为假。可省略其一。如：xxx?yyy 或者 xxx?:zzz。yyy/zzz 均认为是跳转，不需要写>前缀。
+	xxx?yyy:zzz 条件判断语句。yyy为真，zzz为假。可省略其一，省略的直接接下一句；都不省略的，2个条件都接下一句。如：xxx?yyy 或者 xxx?:zzz。yyy/zzz 均认为是跳转，不需要写>前缀。
 HELP
 exit 0;
 }
@@ -25,12 +25,13 @@ $cnt=@color; $cc=0; $end=";\n"; $out=""; @isdia=[];
 open IN,"<$ARGV[0]"; @_=<IN>; close IN;
 $base=$ARGV[0];$base=~s/\..*//;
 @v=map {s".*\Q$sep\E"";s/^\s*//;s/\s*$//;chomp $_;$_} grep /$sep/,@_;
+#for(@v){print "$_\n";}
 #--------------------------------
 for $j (0 .. $#v){
 	$i=$v[$j];
 	next if $i eq "";
-	$i=~s/[\ -\.]/_/g;
-	$i=~s/;/->/g;	#增加;分割的写法
+	$i=~s/[\ -\.]/_/g;	#空格中杠点，都转成下划线。
+	$i=~s/;/->/g;	#增加;分割的写法。判断语句前不能写;
 	if($i=~/>$/){ #入口
 		$out=~s/->$//g; push @output,$out.$end if $out=~/->/;
 		push @output,"}\n" if $cc;
@@ -53,11 +54,22 @@ for $j (0 .. $#v){
 	push @output,$out.$t[0].$end if $out=~/->/;
 	$out="";
 	setshape($t[0],$dend); push @isdia,$t[0];
-	jump($t[1],"y"); jump($t[2],"n");
+
+	my $next=$v[$j+1]; $next=~s/\?.*//;
+	$_=$t[1]||$next; if(/->/){$out="$_->"; s/->.*//;}
+	push @output,"$t[0]:s->$_".'[label="Yes"];'."\n";
+	$_=$t[2]||$next; if(/->/){$out="$_->"; s/->.*//;}
+	push @output,"$t[0]:e->$_".'[label="No" style=dotted];'."\n";
+	if($t[1] && $t[2]){
+		push @output,"$t[1]->$next$end";
+		$out="$t[2]->";
+	}
 }
 #--------------------------------
 for(@output){ # 判断的入口，全部顶部
-	$i=$_; for(@isdia){$i=~s/->$_/->$_:n/;} $_=$i;}
+	$i=$_; for(@isdia){$i=~s/->$_/->$_:n/;} $_=$i;
+	s/->>(.*?)->/->$1;/;	#断开出口语句。‘设置5->>返回->关闭’ 改成 ‘设置5->返回;关闭’
+}
 $out=~s/->$//g; push @output,$out.$end if $out=~/->/;
 #--------------------------------
 unshift @output,"
@@ -68,20 +80,6 @@ push @output,"}\n}\n";
 open OUT,">$base.dot"; print OUT @output;close OUT;
 `dot -T$ext "$base.dot" -o $base.dot.$ext`;
 `eog $base.dot.$ext`;
-#--------------------------------
-sub jump(){
-	my ($_, $YN)=@_;
-	my $x=0; if($_ eq "" || /^>/){$x=1;}	#空或者出口，不自动转下一句
-	my $next=$v[$j+1]; $next=~s/\?.*//;
-	if($_ eq ""){$_=$next;} #为空，分支到下一句
-	s/^>//; #判断分支，都是返回，不管出口标志
-	if($YN eq "y"){push @output,"$t[0]:s->$_".'[label="Yes"];'."\n";}
-	else{push @output,"$t[0]:e->$_".'[label="No" style=dotted];'."\n";}
-	if($x){return;}
-# 跳转分支自动转下一句
-	s/.*->//; #最后一条
-	push @output,"$_->$next;\n";
-}
 #--------------------------------
 sub setshape(){ # 名称，形状
 	my ($name, $shape)=@_;
