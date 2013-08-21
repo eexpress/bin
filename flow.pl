6a4c35269e71a971cd0aa719ee717478c56f994f
eexpress <eexpress@163.com>
2013-08-21 15:48:58 +0800

    flow1.6

--- flow.pl
+++ flow.pl
@@ -1,88 +1,86 @@
 #!/usr/bin/perl
 
-#----------define here-----------
+#--------------------------------
 $ext="svg"; $sep='///'; $font='Vera Sans YuanTi';
 @color=qw(#eecc80 #6495ED #ccee80 #80ccee #eecc80 #80eecc);
-$send='[shape=ellipse]'.$end; $dend='[shape=diamond]'.$end;
-$eend='[shape=egg]'.$end; $hend='[shape=house]'.$end;
+$cnt=@color; $cc=0; $end=";\n"; $out=""; @isdia=[];
+$quitstr="退出";
+$FUNC='[shape=ellipse]'.$end; $JUDGE='[shape=diamond]'.$end;
+$QUIT='[shape=egg]'.$end; $EXIT='[shape=house]'.$end;
 #http://www.graphviz.org/content/node-shapes
+
+$base=$ARGV[0];$base=~s/\..*//;
 #--------------------------------
 if(($#ARGV==-1) || ($ARGV[0]=~/--help|-h/i) || ! -f "$ARGV[0]"){
 print <<HELP;
 AUTHOR:		eexpress
-VERSION:	1.5
+VERSION:	1.6
 USAGE:		flow.pl source_file
 DESCRIPTION:
 自动根据注释里面的///后面的内容，生成流程图。依赖graphviz。
 flow.pl 文件【各类语法的源码，只要注释不和///冲突】
 语法说明：
-	xxx> 表示函数入口。通常是函数名。必须有一个。
-	>xxx 表示函数出口。通常是return或者exit这样的。
+	>xxx 表示函数入口。通常是函数名。第一行缺省为入口。不能带;
 	xxx?yyy:zzz 条件判断语句。yyy为真，zzz为假。可省略其一，如：xxx?yyy 或者 xxx?:zzz。省略的直接接下一句；都不省略的短语，2个条件都接下一句。
+	跳转到某行使用数字表示。比如 xxx?23。使用0表示退出/return/exit。
+	使用;连接多句。
 HELP
 exit 0;
 }
 #--------------------------------
-$cnt=@color; $cc=0; $end=";\n"; $out=""; @isdia=[];
-open IN,"<$ARGV[0]"; @_=<IN>; close IN;
-$base=$ARGV[0];$base=~s/\..*//;
-@v=map {s".*\Q$sep\E"";s/^\s*//;s/\s*$//;chomp $_;$_} grep /$sep/,@_;
-#for(@v){print "$_\n";}
+open IN,"<$ARGV[0]"; while(<IN>){
+	next if ! /$sep/;
+	s/.*$sep\s*//; s/\s*$//;	#去掉首尾空格
+#    s/;/_$.->/g; push @contents, $_."_$."; }
+	push @contents, "$. $_"; }
+close IN;
+#for(@contents){print "$_\n";} exit;
 #--------------------------------
-for $j (0 .. $#v){
-	$_=$v[$j];
-	next if $_ eq "";
-	s/[\ -\.]/_/g;	#空格中杠点，都转成下划线。
-	s/_*;_*/->/g;	#增加;分割的写法。判断语句前不能写;
-	if(/>$/){		#入口
-		$out=~s/->$//g; push @output,$out.$end if $out=~/->/;
-		push @output,"}\n" if $cc;
+for $j (0 .. $#contents){
+	$contents[$j]=~/ /; $line=$`; $_=$';
+	if(/^>/ || ! $j){	#入口。带>或者第一行。不能带;号。
+		if($j){
+		saveout(); setshape($q,$EXIT);
+		push @output,"}\n";	#结束subgraph
+		}
 		push @output, "\nsubgraph{\t".'node [color="'.($color[$cc%$cnt]).'"]'.$end; $cc++;
-		@hasshape=[];
-		$_=~s/>//g; setshape($_,$send);
-		$out="$_->"; next;
-		}
-	if(/^>/){ #出口
-		$_=~s/^>//; setshape($_,$hend);
-		push @output,$out.$_.$end if $out=~/->/;
-		$out='';
-		next;
-		}
-	if (! /\?/){ #常规
-		$out.="$_->"; next;
-		}
-	# 条件判断，包括出口
-	@t=[];@t=split /[?:]/;
-	push @output,$out.$t[0].$end if $out=~/->/;
-	$out="";
-	setshape($t[0],$dend); push @isdia,$t[0];
-#    下句的第一个
-	$_=$v[$j+1]; s/[;?].*//; s/^>//; s/[\ -\.]/_/g;
-	my $next=$_;
-
-	$_=$t[1]||$next; if(/->/){$out=$_; s/->.*//;}
-	if(/^>/){s/^>//; setshape($_,$hend);}
-	push @output,"$t[0]:s->$_".'[label="Yes"];'."\n";
-	$_=$t[2]||$next; if(/->/){$out=$_; s/->.*//;}
-	if(/^>/){s/^>//; setshape($_,$hend);}
-	push @output,"$t[0]:e->$_".'[label="No" style=dotted];'."\n";
-#    退出的情况
-	$_=$out;
-#    if(/^>/){s/^>//; setshape($_,$hend); $out="";}
-	if(/->>/){push @output, $_.$end; s/.*->//; setshape($_,$hend); $out="";}
-	if($out){$out=~s/^>//; $out.="->";}
-#    真假条件都有的短语，均接下一句
-	if($t[1] && $t[2]){
-		$_=$t[1]; s/^>//; push @output,"$_->$next$end";
-		$_=$t[1]; s/^>//; $out="$_->";
+		$q=$quitstr."_$cc";
+		s/^>//g; normal_segment($_);
+			$_.="_$line"; $_="\"$_\"" if /[- .]/;
+		setshape($_,$FUNC); next;
 	}
+	if (! /\?/){ normal_segment($_); next;}
+#--------------------------------
+	# 条件判断
+	($judge,$byes,$bno)=split /[?:]/;
+#    $judge.="_$line"; $_=$judge; $_="\"$_\"" if /[- .]/;
+#    $out.=$_; saveout();
+	$judge=makeup($judge); saveout($judge);
+	setshape($judge,$JUDGE); push @isdia,$judge;
+
+#    下句的第一个段
+#    $_=$contents[$j+1]; s/[;?].*//; /\ /;
+#    $_=$'."_$`"; $_="\"$_\"" if /[- .]/;
+
+	my $next=fetch01seg($contents[$j+1]);
+
+	if($byes ne ""){
+		$goto=fetch01seg("$line $byes");
+		normal_segment($byes); if($bno){ saveout($next); }
+	}else{ $goto=$next; }
+	push @output,"$judge:s->$goto".'[label="Yes"];'."\n";
+	if($bno ne ""){
+		$goto=fetch01seg("$line $bno");
+		normal_segment($bno);
+	}else{ $goto=$next; }
+	push @output,"$judge:e->$goto".'[label="No" style=dotted];'."\n";
 }
 #--------------------------------
 for(@output){ # 判断的入口，全部顶部
 	$in=$_; for(@isdia){$in=~s/->$_/->$_:n/;} $_=$in;
 	s/>>/>/;
 }
-$out=~s/->$//g; push @output,$out.$end if $out=~/->/;
+saveout(); setshape($q,$EXIT);
 #--------------------------------
 unshift @output,"
 digraph G {
@@ -93,10 +91,47 @@ open OUT,">$base.dot"; print OUT @output;close OUT;
 `dot -T$ext "$base.dot" -o $base.dot.$ext`;
 `eog $base.dot.$ext`;
 #--------------------------------
+sub saveout(){
+	my $i=shift;
+	if($i){$out.=$i;}else{$out=~s/->$//;}
+	push @output,$out.$end if $out=~/->/;
+	$out="";
+}
+#--------------------------------
 sub setshape(){ # 名称，形状
 	my ($name, $shape)=@_;
-	$name=~s/>//g;
-	my $has=grep /^\Q$name\E$/,@hasshape;
-	if($has==0){push @hasshape,$name, push @output,"\t".$name.$shape.$end;}
+	my $has=grep /^\Q$name\E$/,@shapegroup;
+	if($has==0){push @shapegroup,$name; push @output,"\t".$name.$shape;}
+}
+#--------------------------------
+sub normal_segment(){
+#    假设$out有数据
+	my @seg=split /;/, shift;
+	for(@seg){
+		if(/\D/){	#非跳转。附加行号，遇特殊字符，引号包括。
+			$_.="_$line"; $_="\"$_\"" if /[- .]/;
+			$out.="$_->"; next;}
+		if($_ eq "0"){
+			push @output,$out.$q.$end if $out=~/->/;
+		}else{
+			my @tmp=grep "$_-",@contents;
+			my @tmp1=split /[-;]/,$tmp[0];
+			push @output,$out."$tmp1[1]_$tmp[0]".$end;
+		}
+		$out=''; break;
+	}
+}
+#--------------------------------
+sub fetch01seg(){
+	$_=shift;
+	s/[?;].*//; /\ /;
+	if($' eq "0"){return $q;}
+	$_=$'."_$`"; $_="\"$_\"" if /[- .]/;
+	return $_;
+}
+#--------------------------------
+sub makeup(){
+	$_=shift; $_.="_$line"; $_="\"$_\"" if /[- .]/;
+	return $_;
 }
 #--------------------------------
