543b55c2e49645bad4cf9fc26e5931afe515550e
eexpress <eexpress@163.com>
2013-08-26 16:19:11 +0800

    htcsock.pl add receive

--- htcsock.pl
+++ htcsock.pl
@@ -8,9 +8,14 @@ open IN,"/proc/net/arp"; @arp=<IN>; close IN;
 } until($d[0]);
 $_=$d[0]; s/\ .*//; chomp; print "sock:\t$_\n";
 
-my $sock = IO::Socket::INET->new(PeerAddr => $_ , PeerPort => 6000, Type=>SOCK_STREAM, Proto=>"tcp") or die "Can not create socket connect.$@\n";  
-print $sock "\x00\x02\x00\x00";
-print $sock "\x00\x03\x00\x00";
+my $sock = IO::Socket::INET->new(PeerAddr => $_ , PeerPort => 6000, Type=>SOCK_STREAM, Proto=>"tcp") or die "socket connect fail. $@\n";  
+
+print $sock "\x00\x02\x00\x00"; receivesock();
+print $sock "\x00\x03\x00\x00"; receivesock();
 print $sock "\x00\x05\x00\x00";
 close $sock;  
 
+sub receivesock{
+local $/=\4;
+print "Receive: --"; print unpack "H*",<$sock>; print "--\n";
+}
