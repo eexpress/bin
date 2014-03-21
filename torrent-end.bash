84fc49b991480ea229c1749495a9d9340a3a3e29
eexpress <eexpress@163.com>
2014-03-21 11:23:41 +0800

    torrent-end.bash

---
+++ torrent-end.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dir="$TR_TORRENT_DIR/$TR_TORRENT_NAME"
+dest="/home/eexp/视频/restricted"
+dest1="/media/eexp/32G/restricted/"
+if [ -d $dest1 ]; then dest=$dest1; fi
+cd $dir
+mv *.avi *.mp4 *.wmv $dest
+[ $? != 0 ] && exit
+cd ..
+rm -r $TR_TORRENT_NAME
+cd $dest
+for i in *.avi *.mp4 *.wmv; do
+rename 's/3xplanet_//;s/^[\d\.]*-//' $i
+done
+
