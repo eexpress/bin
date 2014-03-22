9a1e5f89bdb05de0417ca5e1aa651cab0ca38417
eexpress <eexpress@163.com>
2014-03-22 22:16:05 +0800

    diff-scp.bash

--- diff-scp.bash
+++ diff-scp.bash
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-dest=$(echo 'eexp@'$(avahi-browse -at|grep -v `hostname`|grep v4|awk '{print $4;}')'.local')
+remote=$(avahi-browse -at|grep v4|grep -v `hostname`|awk '{print $4;}'|grep -i "$1")
+[ -z $remote ] && echo "没有发现远程机器。" && exit
+echo $remote|grep '\ ' >/dev/null
+[ $? -eq 0 ] && echo "发现多于一台的远程机器，请指定特征字符串。" && exit
+
+dest=$(echo 'eexp@'$remote'.local')
+echo $dest
 dir=`pwd`
 
+echo "-----------------------"
 ls -1>/tmp/this
 ssh $dest ls -1 $dir |diff /tmp/this -|grep '^<'|sed 's/^<\s*//'>/tmp/scp.list
+cnt=`cat /tmp/scp.list |wc -l`
+[ $cnt -eq 0 ] && echo "本地目录没有发现比远程目录更多的文件。" && exit
 cat /tmp/scp.list
 echo "-----------------------"
-echo "确认执行，请按空格键/回车键。其他键取消。"
+echo "复制当前目录的文件到远程相同目录，确认执行，请按空格键/回车键。其他键取消。"
 read -s -n 1 y
 if [[ $y == '' ]]; then
 	cat /tmp/scp.list |while read i ; do scp $i $dest:$dir; done
