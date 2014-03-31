14659f2fadc59c307ff998e1a1d43815d2687756
eexpress <eexpress@163.com>
2014-03-31 20:03:34 +0800

    diff-scp.bash

--- diff-scp.bash
+++ diff-scp.bash
@@ -16,7 +16,7 @@ cnt=`cat /tmp/scp.list |wc -l`
 [ $cnt -eq 0 ] && echo "本地目录没有发现比远程目录更多的文件。" && exit
 cat /tmp/scp.list
 echo "-----------------------"
-echo "复制当前目录的文件到远程相同目录，确认执行，请按空格键/回车键。其他键取消。"
+echo "复制当前目录新增的文件到远程相同目录，确认执行，请按空格键/回车键。其他键取消。"
 read -s -n 1 y
 if [[ $y == '' ]]; then
 	cat /tmp/scp.list |while read i ; do scp $i $dest:$dir; done
