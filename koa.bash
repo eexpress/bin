c4615d88a10c478cf7710fc2f0929ee0f1c896a8
eexpress <eexpress@163.com>
2014-03-31 16:14:03 +0800

    koa.bash

---
+++ koa.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rdesktop -u exp -p exp -g 1000x600 192.168.100.190
--- torrent-end.bash
+++ torrent-end.bash
@@ -6,7 +6,7 @@ dest1="/media/eexp/32G/restricted/"
 if [ -d $dest1 ]; then dest=$dest1; fi
 cd $dir
 mv *.avi *.mp4 *.wmv $dest
-[ $? != 0 ] && exit
+#[ $? != 0 ] && exit
 cd ..
 #rm -r $TR_TORRENT_NAME
 gvfs-trash $TR_TORRENT_NAME
