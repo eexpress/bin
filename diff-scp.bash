bb9068c3a5f0d67ca4e2d18b7dcbc523c3995986
eexpress <eexpress@163.com>
2014-04-07 18:39:59 +0800

    diff-scp.bash only use to remote for scp

--- diff-scp.bash
+++ diff-scp.bash
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-#参数1，直接指定远程目录，或者指定远程机器名的关键词（使用相同路径）
-if [ -d $1 ]; then
-	dest=$1
-else
-	remote=$(avahi-browse -at|grep v4|grep -v `hostname`|awk '{print $4;}'|grep -i "$1")
-	[ -z $remote ] && echo "没有发现远程机器。" && exit
-	echo $remote|grep '\ ' >/dev/null
-	[ $? -eq 0 ] && echo "发现多于一台的远程机器，请指定特征字符串。" && exit
+echo -n "find remote ... "
+remote=$(avahi-browse -at|grep v4|grep -v `hostname`|awk '{print $4;}'|grep -i "$1")
+[ -z $remote ] && echo "没有发现远程机器。" && exit
+echo $remote|grep '\ ' >/dev/null
+[ $? -eq 0 ] && echo "发现多于一台的远程机器，请指定特征字符串。" && exit
 
-	dest=$(echo 'eexp@'$remote'.local')
-	echo $dest
-fi
+dest=$(echo 'eexp@'$remote'.local')
+echo $dest
+[ -z $dest ] && exit
 
 echo "-----------------------"
 dir=`pwd`
@@ -21,7 +18,7 @@ cnt=`cat /tmp/scp.list |wc -l`
 [ $cnt -eq 0 ] && echo "本地目录没有发现比远程目录更多的文件。" && exit
 cat /tmp/scp.list
 echo "-----------------------"
-echo "复制当前目录新增的文件到远程相同目录，确认执行，请按空格键/回车键。其他键取消。"
+echo "复制当前目录新增的文件到远程相同目录($dest:$dir)，确认执行，请按空格键/回车键。其他键取消。"
 read -s -n 1 y
 if [[ $y == '' ]]; then
 	cat /tmp/scp.list |while read i ; do scp $i $dest:$dir; done
