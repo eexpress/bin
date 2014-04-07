08f9e4a71169360820d81ef3a4084b24c6ac4fdc
eexpress <eexpress@163.com>
2014-04-07 17:18:21 +0800

    diff-scp.bash+xsel-sento.bash

--- diff-scp.bash
+++ diff-scp.bash
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-remote=$(avahi-browse -at|grep v4|grep -v `hostname`|awk '{print $4;}'|grep -i "$1")
-[ -z $remote ] && echo "没有发现远程机器。" && exit
-echo $remote|grep '\ ' >/dev/null
-[ $? -eq 0 ] && echo "发现多于一台的远程机器，请指定特征字符串。" && exit
+#参数1，直接指定远程目录，或者指定远程机器名的关键词（使用相同路径）
+if [ -d $1 ]; then
+	dest=$1
+else
+	remote=$(avahi-browse -at|grep v4|grep -v `hostname`|awk '{print $4;}'|grep -i "$1")
+	[ -z $remote ] && echo "没有发现远程机器。" && exit
+	echo $remote|grep '\ ' >/dev/null
+	[ $? -eq 0 ] && echo "发现多于一台的远程机器，请指定特征字符串。" && exit
 
-dest=$(echo 'eexp@'$remote'.local')
-echo $dest
-dir=`pwd`
+	dest=$(echo 'eexp@'$remote'.local')
+	echo $dest
+fi
 
 echo "-----------------------"
+dir=`pwd`
 ls -1>/tmp/this
 ssh $dest ls -1 $dir |diff /tmp/this -|grep '^<'|sed 's/^<\s*//'>/tmp/scp.list
 cnt=`cat /tmp/scp.list |wc -l`
--- xsel-sento.bash
+++ xsel-sento.bash
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 f=`xsel -o|tr '\n' ' '|sed 's/\ +/ /g'`
-cmd=$*" "$f
+#参数中可使用+号替换xsel结果
+echo "$*"|grep '\+'
+if [ $? == 0 ]; then
+	cmd=`echo $*|sed -e "s'\+'$f'"`
+else
+	cmd=$*" "$f
+fi
 echo $cmd
 echo "-----------------------"
 echo "确认执行，请按空格键/回车键。其他键取消。"
