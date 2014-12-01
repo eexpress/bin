dd4c13a45ce72321979acdd405d32c92b386af63
eexpress <eexpress@163.com>
2014-12-01 08:55:04 +0800

    proxy

--- ssh.proxy.bash
+++ ssh.proxy.bash
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-ssh -qTfnN -D 7070 root@128.199.153.182
+#oneleaf
+#ssh -qTfnN -D 7070 root@198.71.90.209
+
+#imadper
+#ssh -qTfnN -D 7070 root@128.199.153.182
+#ssh -qTfnN -D 7070 eexp@104.236.176.123
+ssh -qTfnN -D 7070 eexp@128.199.153.182
 
