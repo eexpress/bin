e40498116c010a163292a9fadd70af750be64e5b
eexpress <eexpress@163.com>
2014-12-02 08:59:44 +0800

    ssh.proxy.bash

--- ssh.proxy.bash
+++ ssh.proxy.bash
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 #oneleaf
 #ssh -qTfnN -D 7070 root@198.71.90.209
 
 #imadper
 #ssh -qTfnN -D 7070 root@128.199.153.182
 #ssh -qTfnN -D 7070 eexp@104.236.176.123
-=======
->>>>>>> 8730592c74a75d3f13f970e4794a084360bc6517
 ssh -qTfnN -D 7070 eexp@128.199.153.182
 
