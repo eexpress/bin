29799bac72c268329ab993c5292c9d858fabeb70
eexpress <eexpress@163.com>
2014-12-03 13:40:23 +0800

    dig.bash

--- dig.bash
+++ dig.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+url="twitter.com"
+url="google.com"
 re='\<IN\>.*[0-9]\{1,3\}'
-echo SSH
-dig @128.199.153.182 twitter.com|grep "$re"
-echo 114
-dig @114.114.114.114 twitter.com|grep "$re"
-echo 8888
-dig @8.8.8.8 twitter.com|grep "$re"
-echo hosts
-grep "\stwitter.com" /etc/hosts
+echo -e "===>\t SSH"
+dig @128.199.153.182 $url|grep "$re"
+echo -e "===>\t 114"
+dig @114.114.114.114 $url|grep "$re"
+echo -e "===>\t 8888"
+dig @8.8.8.8 $url|grep "$re"
+echo -e "===>\t hosts"
+grep "\s$url" /etc/hosts
