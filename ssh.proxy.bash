f13260cd98bb3ff19e56a8d5996eddbfa3534b9c
eexpress <eexpress@163.com>
2015-01-22 08:49:00 +0800

    ssh.proxy.bash

--- ssh.proxy.bash
+++ ssh.proxy.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+c=1
 while : ; do
 	ip route|grep default
-	[[ $? ]] && break
+	[[ $? -eq 0 ]] && break
 	echo waiting net...
+	((c++))
+	[[ $c -gt 5 ]] && msg "ssh fault." && exit
 	sleep 2
 done
 
@@ -18,3 +21,4 @@ p=`pgrep -x -d ' ' ssh -u $(whoami)`
 #ssh -qTfnN -D 7070 eexp@104.236.176.123
 ssh -qTfnN -D 7070 eexp@128.199.153.182
 
+[[ $? -eq 255 ]] && msg "ssh error." || msg "ssh established."
