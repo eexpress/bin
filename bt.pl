23296d9a265144199f09558e515b6c8b85485111
eexpress <eexpress@163.com>
2015-02-05 10:27:38 +0800

    bt.pl change to support https

--- bt.pl
+++ bt.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
 
 use utf8;
-use LWP::Simple qw($ua get);
+use open qw(:std :utf8);
+use LWP::UserAgent;
+my $ua = LWP::UserAgent->new( agent => 'Opera/9.80', timeout => 10 );
 use Net::DBus;
 my $bus = Net::DBus->session->get_service('org.freedesktop.Notifications')->get_object('/org/freedesktop/Notifications','org.freedesktop.Notifications');
 #输入编号
@@ -11,70 +13,43 @@ m/\w{2,4}-\d{3,4}/; $s=$&;
 die "not correct id." if !$s;
 print "> $s <\n";
 open OUT,">>/tmp/bt.log"; print OUT ". $s .\n"; close OUT;
-$ua->timeout(10);
 #--------------------------------------------
-$web="http://thepiratebay.to";
-$url="$web/search/?FilterStr=$s&ID=&Limit=800&Letter=&Sorting=DDate";
-print "1 ->\t$url\n"; $_ = get($url);
-#die "Couldn't get it!" unless defined $_;
-if(defined $_){
-#<a href="/torrent/9278096/IPZ-260 Erika Shibasaki JAV CENSORED"
-#<a href="http://thepiratebay.to/torrent/1594775/MOND-013%20amateur%20JAV%20CENSORED/" onclick=
-	/href=\"$web\/torrent\/.*?\"/; $_=$&; s/href=//; s/\"//g; s/\ /%20/g;
-	if($_=~m'/'){
-		$url=$_;
-		print "2 ->\t$url\n"; $_ = get($url);
-		/magnet:[^"]*/; $_=$&; $_.="\n";
+$web="https://thepiratebay.se";
+$url="$web/search/$s/";
+print "1 ->\t$url\n"; $response = $ua->get($url);
+if ( $response->is_success ) {
+	$_=$response->decoded_content;
+	/magnet:[^"]*/; $_=$&; $_.="\n"; print;
+	if(/magnet/){
 		`transmission-remote -a "$_"`;
-	if($?>0){open OUT,">>$ENV{HOME}/magnet.list"; print OUT "--\t$s\n$_"; close OUT; print "Add to transmission retrun error: $?. Save magnet to ~/magnet.list.\n";}
-		print;
-	if($?>0){$bus->Notify("bt", 0, "sunny", "Add to transmission retrun error: $?. Save magnet to ~/magnet.list.\n", "", [], { }, -1);}
+		if($?>0){open OUT,">>$ENV{HOME}/magnet.list"; print OUT "--\t$s\n$_"; close OUT; print "Add to transmission retrun error: $?. Save magnet to ~/magnet.list.\n";}
+		if($?>0){$bus->Notify("bt", 0, "sunny", "Add to transmission retrun error: $?. Save magnet to ~/magnet.list.\n", "", [], { }, -1);}
 		exit;
 	}
 }
 #--------------------------------------------
+#No BT link now
 $url="http://blog.jav4you.com/?s=$s";
-print "1 ->\t$url\n"; $content = get($url);
-#http://blog.jav4you.com/?s=MDYD-866&x=0&y=0
-#Read the rest of this entry »
-die "Couldn't get it!" unless defined $content;
-$content=~/href=.*?Read the rest of this entry/;
+print "1 ->\t$url\n"; $response = $ua->get($url);
+die "Couldn't get it!" unless $response->is_success;
+$_=$response->decoded_content;
+/href=.*?Read the rest of this entry/;
 #<a href="http://blog.jav4you.com/2014/02/mdyd-866-real-wives-only-non-fictional-trip-of-cheating-3/#more-36500" class="more-link">Read the rest of this entry »</a>
 $_=$&; m'http://[^"]*';
-$url=$&; print "2 ->\t$url\n"; $content = get($url);
-die "Couldn't get it!" unless defined $content;
+$url=$&; print "2 ->\t$url\n"; $response = $ua->get($url);
+die "Couldn't get it!" unless $response->is_success;
+$_=$response->decoded_content;
 #<p><b>BitTorrent File</b><br><a href="http://l.jav4you.com/1eQ2eLT" target="_blank" rel="nofollow">ishrhndug.html</a><br><br><br></p>
 #open OUT,">>$ENV{HOME}/bt.log"; print OUT $content; close OUT;
-$_="";
-$content=~/BitTorrent File.*html/;
+/BitTorrent File.*html/;
 $_=$&; s/.*>//;
 #$url="http://www.21stp.com/$_";
-if($_==""){die "No BT link.";}
+if($_==""){
+	$bus->Notify("bt", 0, "sunny", "No BT link found.", "", [], { }, -1);
+	die "No BT link found.";
+}
 $url="http://www.2121.club/$_";
 print "3 ->\t$url\n";
 `gnome-open \'$url\'`; exit;
+#--------------------------------------------
 
-$content = get($url);
-die "Couldn't get it!" unless defined $content;
-#<a href="http://www.21stp.com/save/857470376/dd82969aa8">ダウンロード</a>
-$content=~/href=.*?ダウンロード/;
-$_=$&;
-#网站修改了简单的js
-#m'http://[^"]*';
-m'/save[^\']*';
-$url="http://www.2121.club$&"; print "4 ->\t$url\n";
-`gnome-open \'$url\'`;
-
-#$s="$ENV{HOME}/$s.torrent";
-#if(is_success(getstore($url,$s))){ print "5 ->\tsave to $s\n"; }
-
-#$content = get($url);
-#die "Couldn't get it!" unless defined $content;
-#open OUT,">>$s"; print OUT $content; close OUT;
-
-#use WWW::Mechanize;
-#my $mech = WWW::Mechanize->new(agent=>'Opera/9.80 (X11; Linux i686; U; en) Presto/2.6.30 Version/10.60');
-#$mech -> get($url);
-#if ($mech->success()) { $mech->save_content($s); print "5 ->\tsave to $s\n"; }
-#`transmission-remote -a "$_"`;
-#if($?>0){print "Add to transmission retrun error: $?.\n";}
