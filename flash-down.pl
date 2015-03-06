0778d6d53516a1250b8cc5f616346fbd4c903f55
eexpress <eexpress@163.com>
2015-03-06 21:06:34 +0800

    flash-down.pl 1.3

--- flash-down.pl
+++ flash-down.pl
@@ -13,12 +13,13 @@ GetOptions('view'=>\$view,'help'=>\$help);
 if($help){
 print <<HELP;
 AUTHOR:         eexpress
-VERSION:        1.2
+VERSION:        1.3
 USAGE:          flash-down.pl [--view] [--help] url
 HELP
 exit 0;
 }
 
+#使用pid机制，可以并行执行，自动等待前一个下载完成后继续下载。
 use File::Pid;
 my $pidfile=File::Pid->new({file=>'/tmp/flash-down.pid'});
 if(my $num=$pidfile->running){print $pidfile->program_name." is already running with PID: $num\nwait....\n";}
@@ -40,46 +41,40 @@ $_="http://www.flvcd.com/parse.php?flag=&format=high&kw=$_&sbt=%BF%AA%CA%BCGO%21
 
 my $bus = Net::DBus->session->get_service('org.freedesktop.Notifications')->get_object('/org/freedesktop/Notifications','org.freedesktop.Notifications');
 my $mech = WWW::Mechanize->new(agent=>'Opera/9.80 (X11; Linux i686; U; en) Presto/2.6.30 Version/10.60');
-#my $mech = WWW::Mechanize->new();
 $mech -> get($_);
 if ($mech->success()) {
 	$_=$mech->content();
 	if(/网络限制/){
 		/网络限制.*\.\.\./; $_=$&; s/<br\/>.*$//; 
 		$bus->Notify("flash", 0, "error", "flash 解析失败", "$_\n:(", [], { }, -1);
-	} else {
-	if(/当前解析视频.*?<strong/s){
+	} elsif(/当前解析视频.*?<strong/s){
 	$_=$&; 
 	s/^.*?>//;s/<.*$//s;s/\s*//g;
 	print "$red$bold".$_."$normal==================\n";
 	my $name="$_";
 	my @alllink=$mech->find_all_links(text_regex => qr/http:\/\/.*[0-9a-fA-F]*/,);
 #    delete link with blog.flvcd.com
-#    foreach(@link){
-#        delete if /blog.flvcd.com/;
-#    }
-	my @link=grep(!/blog\.flvcd\.com/,@alllink);
+	my @link=();
+	foreach(@alllink){
+		my $add=$_->url();
+		next if $add=~/blog\.flvcd\.com/;
+		push @link,$add;
+	}
 	my $size=@link;
 	$bus->Notify("flash", 0, "sunny", "$name", "共获取 $size 个地址。", [], { }, -1);
 	print "\e]2;flash-down.pl_下载_$name\a";
-	print map "=> $green".$_->url()."$normal\n",@link;
-print $alllink[0];
-	$pidfile->remove; exit;
-#    open(LINK,">link.log"); print LINK map $_->url()."\n",@link; close LINK;
+	print map "=> $green".$_."$normal\n",@link;
 	my $cnt=1; my $proc="▭"x$size;
 #---------------------	
 if($view){
-	foreach(@link){
-		my $add=$_->url();
+	foreach my $add (@link){
 		`mplayer -quiet $add`;
 		}
-		$pidfile->remove;
-		exit;
+		$pidfile->remove; exit;
 }
 #---------------------	
 	if($size>4){mkdir "$_"; chdir "$_";}
-	foreach(@link){
-		my $add=$_->url();
+	foreach my $add (@link){
 #        print "$red 下载$normal => $green$add$normal\n";
 		my $file=$size>1?sprintf "%s-%02d.flv",$name,$cnt:"$name.flv";
 #        or use try/catch in Try::Tiny
@@ -104,7 +99,6 @@ if($view){
 #    chdir '..';
 	`echo "$ARGV[0]\t《$_》\t结果：$proc">>/tmp/flash-down.log`;
 	`paplay "/usr/share/sounds/ubuntu/stereo/service-login.ogg"`;
-	}
 	}
 } else {
 	$bus->Notify("flash", 0, "gtk-cancel", '网页无效', ':(', [], { }, -1);
