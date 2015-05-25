d8e6789bd20eb0b3ad9ea6364dee888f993e130d
eexpress <eexpress@163.com>
2015-05-25 23:14:26 +0800

    fetch-cache.bash

--- fetch-cache.bash
+++ fetch-cache.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 new="/tmp/findnew"
-size="+2000k"
+size="+5000k"
 format='Audio|MPEG|ASF|Flash'
 
 cd
 touch $new
 read -p "开始监视缓冲。。视频传输完成后，按回车。"
-find ~/.cache/opera-developer/Cache/ -iname "f_*" -newer $new -size $size|while read i; do file $i|egrep $format; [ $? -eq 0 ] && mv $i ~/`basename -z $i`.flv; done
+find ~/.cache/opera-developer/Cache/ -iname "f_*" -newer $new -size $size|while read i; do file $i|egrep $format; [ $? -eq 0 ] && mv $i ~; done
 #find ~/.opera/cache*/ -iname "opr*.tmp" -newer $new -size $size|while read i; do file $i|egrep $format; [ $? -eq 0 ] && mv $i ~/`basename -z $i`.flv; done
 #find ~/.cache/mozilla/firefox/ -iregex ".*/[0-9A-F]*" -newer $new -size $size|while read i; do file $i|egrep $format; [ $? -eq 0 ] && mv $i ~/`basename -z $i`.flv; done
 rm $new
