d0893ff645deb6d854c2990bd8a74aa07a864f55
eexpress <eexpress@163.com>
2015-08-19 14:19:58 +0800

    showport.bash

--- port.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo netstat -antp|awk '/LISTEN/&&!/::/ {split($4,p,":"); split($7,n,"/"); printf ("%s\t%10s\tPID: %s\n",p[2],n[2],n[1])}'
-#sudo netstat -antp|awk '/LISTEN/&&!/::/ {split($4,p,":"); split($7,n,"/"); print p[2] "\t" n[2] "\t\tPID: " n[1]}'
-
---
+++ showport.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo netstat -antp|awk '/LISTEN/&&!/::/ {split($4,p,":"); split($7,n,"/"); f=("/proc/"n[1]"/cmdline"); getline cmd<f; gsub(/\0/," ",cmd); printf ("%s\t%10s\tPID: %s\tCMD: %s\n",p[2],n[2],n[1],cmd)}'
+#sudo netstat -antp|awk '/LISTEN/&&!/::/ {split($4,p,":"); split($7,n,"/"); print p[2] "\t" n[2] "\t\tPID: " n[1]}'
+
