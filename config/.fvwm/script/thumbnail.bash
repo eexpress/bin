bd841bc4182441b07ca2d21a33c5ab73b0085b11
eexpress <eexpress@163.com>
2016-09-25 17:14:30 +0800

    Thumbnail

---
+++ config/.fvwm/script/thumbnail.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# $[w.id] $[w.resource]
+echo $*>>/tmp/id+resource
+id=$1
+name=${2%-server}
+ficon="$HOME/.fvwm/icon/$name.png"
+fthumb="/tmp/thumb.$id.png"
+fout="/tmp/show.$id.png"
+
+xwd -silent -id $id | convert -scale 100 xwd:- png:$fthumb
+
+if [ ! -f "$ficon" ]; then
+	floc=`locate -AweLi -n 1 48x48 $name.png`
+	if [ -f "$floc" ]; then
+		cp "$floc" $ficon
+	else
+		floc=`locate -AweLi -n 1 64x64 $name.png`
+		if [ -f "$floc" ]; then
+			convert -scale 48 $floc $ficon
+		fi
+	fi
+fi
+
+if [ -f "$ficon" ]; then
+	composite -geometry +50+6 $ficon $fthumb $fout
+else
+	cp $fthumb $fout
+fi
+
+echo WindowStyle IconOverride, Icon $fout || echo Nop
