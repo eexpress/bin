af30267663b3a88550d3456c569081c10d53db38
eexpress <eexpress@163.com>
2016-09-25 18:03:55 +0800

    Mouse Scroll on rhythmbox Icon

--- config/.fvwm/script/thumbnail.bash
+++ config/.fvwm/script/thumbnail.bash
@@ -13,10 +13,12 @@ xwd -silent -id $id | convert -scale 100 xwd:- png:$fthumb
 if [ ! -f "$ficon" ]; then
 	floc=`locate -AweLi -n 1 48x48 $name.png`
 	if [ -f "$floc" ]; then
+		echo $floc>>/tmp/id+resource
 		cp "$floc" $ficon
 	else
 		floc=`locate -AweLi -n 1 64x64 $name.png`
 		if [ -f "$floc" ]; then
+			echo $floc>>/tmp/id+resource
 			convert -scale 48 $floc $ficon
 		fi
 	fi
