d198e64426ba219afdf1c6e8e826ab0040982b5a
eexpress <eexpress@163.com>
2016-10-04 00:12:50 +0800

    show-netspeed

--- config/.fvwm/script/show-netspeed.bash
+++ config/.fvwm/script/show-netspeed.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-interval=10
 prev=`cat /sys/class/net/wlan0/statistics/rx_bytes`
-sleep $interval
-curr=`cat /sys/class/net/wlan0/statistics/rx_bytes`
-
-echo $curr $prev $interval|awk '{s=($1-$2)/$3/1024; if(s>=1){print s" KB/s";}else{s=s/1024; print s" MB/s";}}'
+for (( i = 0; i < 10 ; i ++ ))
+do
+	sleep 1
+	curr=`cat /sys/class/net/wlan0/statistics/rx_bytes`
+	echo $curr $prev|awk '{s=($1-$2)/1024; if(s>=1){print s" KB/s";}else{s=s/1024; print s" MB/s";}}'
+	prev=$curr
+done
