ef8600d8ffe67a8dde08635bad2390121c0c153c
eexpress <eexpress@163.com>
2016-11-03 10:43:50 +0800

    baike.pl

--- bot/baike.pl
+++ bot/baike.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl
-use LWP;
+
 use LWP::UserAgent;
-use Encode;
-use POSIX qw(isatty);
+
 my $ua = LWP::UserAgent->new();
 $ua->max_size( 180 * 1024 );
 $in=`xsel -o`;
@@ -12,13 +11,15 @@ $icon='/usr/share/icons/gnome/48x48/actions/system-search.png';
 my $reply = $ua->get("http://baike.baidu.com/item/$in");
 my $html;
 if($reply->is_success){
-		$html = $reply->content;
-		$html=~/"lemmaSummary">(.*?)<div class="configModuleBanner"/s;
+	$html = $reply->content;
+	$html=~/"lemmaSummary">(.*?)<div class="configModuleBanner"/s;
+	if ($1){
 		$_=$1;
 		s/<.*?>//g; s/\r//g; s/&.*?;//g; s/\n//g;
-		if (isatty(0)){
+		if (POSIX::isatty(0)){
 			print; print "\n";
 		} else {
 			`notify-send -i $icon "$in" "$_"`;
 		}
+	}
 }
