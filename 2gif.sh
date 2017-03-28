dfd716ff92fea4451aa5586244ce96b89443d268
eexpress <eexpress@163.com>
2017-03-28 16:26:38 +0800

    nautilus+scripts

--- 2gif.sh
+++
@@ -1,5 +0,0 @@
-palette="/tmp/palette.png"
-filters="fps=15,scale=600:-1:flags=lanczos"
-
-ffmpeg -i $1 -vf "$filters,palettegen" -y $palette
-ffmpeg -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $1.gif
--- 2mp3.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mplayer -vo null -ao pcm $1
-lame audiodump.wav $1.mp3
-rm audiodump.wav
-
