ac223f45c77ed9f45f547214482b0cfdbb27ca53
eexpress <eexpress@163.com>
2017-05-08 16:11:54 +0800

    wine.sh

--- wine.sh
+++ wine.sh
@@ -4,11 +4,13 @@ m=`echo $info|cut -d ' ' -f4|sed 's/\+.*//'`
 echo "<$o>----<$m>"
 n=1024x768
 d=`dirname "$1"`
-echo $d
+#echo $d
 cd "$d"
 e=`basename "$1"`
 xrandr --output $o --mode $n
-echo change to $n
+#echo change to $n
+XMODIFIERS=@im=xim
 wine explorer /desktop=foo,$n "$e"
+#wine "$e"
 xrandr --output $o --mode $m
-
+XMODIFIERS=@im=ibus
