cb9884c33b51b662e241f8e40b982756c4604f5b
eexpress <eexpress@163.com>
2017-05-18 00:36:45 +0800

    ip138死掉，换chinaz

--- monitor-clip.pl
+++ monitor-clip.pl
@@ -1,20 +1,23 @@
 #!/usr/bin/perl
 
+use utf8;
+
 #----------------------------------
-sub ip_138{
+sub ip_domain{
 	use LWP::Simple;
-	use Encode qw(encode decode);
+	print "\e[1;34m check ip \e[0m\n";
 	$in=shift;
 	$icon='/usr/share/icons/HighContrast/48x48/actions/system-search.png';
-	$url="http://www.ip138.com/ips138.asp?ip=".$in;
-	$_=get($url); $_=encode("utf8",decode("gbk",$_));
-	/本站数据：([^<]*)/;
-	$_=$1;
-	print;
+	$url="http://ip.chinaz.com/".$in;
+	$_=get($url);
+	/IP的物理位置(.*?)<div/s;
+	$_=$1; s'<.+?>''g; s'[\ \r]''g; s'\n+'\r'g;
+	`echo "$_">/tmp/ip`;
 	`notify-send -i $icon "IP地址查询 $in" "$_"`;
 }
 #----------------------------------
 sub web_translate{
+	print "\e[1;34 translate \e[0m\n";
 	$in=shift;
 	$in=~s/["']//g;
 	if($in=~/[\x80-\xFF]{2,4}/){$str="#zh/en";}else{$str="#en/zh";}
@@ -25,6 +28,7 @@ sub web_translate{
 #----------------------------------
 sub videoplay{
 	$in=shift;
+	print "\e[1;34 video \e[0m\n";
 	print $in;
 	chomp $in;
 	$dl='/home/eexpss/bin/you-get/you-get';
@@ -46,26 +50,32 @@ sub exec_regex{
 #百度盘的地址，下载。终端里执行和面板点击都有效，就是热键失效？！！
 	if(/^https*.+baidupcs.com\/.+/)
 	{sound();chomp;`gnome-terminal -x axel -n 120 -a '$_'`;return;}
+#-----------------
 #视频网站，直接播放。
 	if($_=~m!^https*://(v.youku.com|tv.sohu.com|video.tudou.com|v.qq.com|www.iqiyi.com|www.bilibili.com|www.acfun.cn)!)
 	{sound();videoplay($_);return;}
+#-----------------
 #/和~开头的存在的文件，打开
 	if(/^~?\/.../)
 	{s/^~/$ENV{HOME}/;s/\n.*//;if(-e){sound();`xdg-open "$_"`;return;}}
+#-----------------
 #终端选择的视频文件(文件名可以不完整)，定位并播放
 	if(/\.(avi|mkv|mp4|wmv|ogg|flv|jpg|png|svg)\'*$/)
 	{$_=`locate -e -n 1 $_`;chomp;if(-e){sound();`xdg-open "$_"`;return;}}
-#ip格式的数字，域名，查询
-	if(/\d+\.\d+\.\d+\.\d+/){sound();ip_138($&);return;}
-	if(/(\w+\.){1,3}(com|net|org|edu|gov|biz|tel|info|name|[a-z]{2})$/ && !/:/)
-	{sound();ip_138($&);return;}
-#    if(/(\w+\.){1,3}[A-Za-z]{2,3}$/ && !/:/){sound();ip_138($&);return;}
+#-----------------
+#ip地址，域名
+	if(/\d+\.\d+\.\d+\.\d+/){sound();ip_domain($&);return;}
+	if(/(\w+\.){1,3}(com|net|org|edu|gov|biz|tel|info|name|cc)$/ && !/:/)
+#    if(/(\w+\.){1,3}(com|net|org|edu|gov|biz|tel|info|name|[a-z]{2})$/ && !/:/)
+#    if(/(\w+\.){1,3}[A-Za-z]{2,3}$/ && !/:/)
+	{sound();ip_domain($&);return;}
+#-----------------
+	if(-f "/tmp/fanyi"||$mode eq "parse"){	#使用临时文件，开关翻译分支。
 #全英文句子或单词，有本地翻译软件就直接翻译，否则网页翻译
 # 可显示ascii字符，全英文句子。“.xxx”像文件名的，不执行。
-	if(/^[\x20-\x7e]+$/ && /\w{3,}/ && !/\.\w+$/)
-	{sound();web_translate($_);return;}
-#番号下载
-	if(/\w{2,4}-\d{3,4}/){`/home/eexp/bin/bt.pl $&`;return;}
+		if(/^[\x20-\x7e]+$/ && /\w{3,}/ && !/\.\w+$/)
+		{sound();web_translate($_);return;}
+	}
 }
 #----------------------------------
 sub sound{
@@ -81,6 +91,7 @@ sub sound{
 }
 #==================================
 #parse mode <- any para here
+$mode="parse";
 if($ARGV[0]){
 	$_=`xclip -o`;
 	exec_regex($_);
@@ -88,6 +99,7 @@ if($ARGV[0]){
 }
 
 #monitor mode
+$mode="monitor";
 $oldclip=`xclip -o`;
 while(true){
 	sleep 2;
