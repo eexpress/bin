50fbcbe5559af985f298dbaa67b02d48f2fe1d5e
eexpress <eexpress@163.com>
2017-05-22 22:59:35 +0800

    试试dnscrypt

---
+++ btsync.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/eexpss/bin/Resilio-Sync/
+./rslsync --config sync.conf
+
--- config/.local+share+nautilus+scripts/目录用量.bash
+++ config/.local+share+nautilus+scripts/目录用量.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 dir=`echo -n $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS|head -n 1|sed 's/\x0A//g'`
-[ -f $dir ] && dir=`dirname $dir`
+[ -f "$dir" ] && dir=`dirname "$dir"`
 baobab "$dir"
