954ea1e18c15cf51e778730e2ac64f5856584392
eexpress <eexpress@163.com>
2019-01-12 16:23:59 +0800

    ln.bash

--- config/ln-全部隐藏文件到家目录.bash
+++ config/ln-全部隐藏文件到家目录.bash
@@ -3,10 +3,13 @@
 cd "$(dirname "$0")"
 d=`pwd`
 for i in `ls -A|grep '^\.'`; do
+if [[ $i =~ '.swp' ]]; then continue; fi
 # 路径中，+表示斜杠，=表示空格，\后面的是注释，全删除。
-l=`echo $i|sed 's/\\.*//; s.+./.g; s.=.\\\\ .g'`
-cmd="rm ~/$l; ln -sf $d/$i ~/$l"
-echo $cmd
+l=`echo $i|sed 's.+./.g; s.=.\\\\ .g'`
+#l=`echo $i|sed 's/\\.*//; s.+./.g; s.=.\\\\ .g'`
+cmd="rm -r ~/$l; ln -sf $d/$i ~/$l"
+#echo $cmd
+printf "%24s\t-->\t%s\n" "~/$l" $d/$i
 eval $cmd
 done
 #find . -maxdepth 3 -type l -exec rm {} \;
