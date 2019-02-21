c1769176be92e6d3ffe59a477ab75bdf204ab63d
eexpress <eexpress@163.com>
2019-02-21 11:38:34 +0800

    transmission-auto-add-trackers.bash+1

--- transmission-auto-add-trackers.bash
+++ transmission-auto-add-trackers.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 file="$HOME/bin/config/BT_trackers_best.txt"
-#wget https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt -O $file
+
+[ -f $file ] || wget https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt -O $file
 
 cmd="transmission-remote"	# 需要打开 transmission 的 remote control。
 line=`$cmd -l|wc -l`
