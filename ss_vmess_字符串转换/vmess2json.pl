694f541529f1d5402d1247b098e2434c3d783cde
eexpress <eexpress@163.com>
2019-09-25 15:03:30 +0800

    o+vmess2json.pl

---
+++ ss_vmess_字符串转换/vmess2json.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+use 5.010;
+use MIME::Base64;
+use JSON;
+
+$_=shift; s'^vmess://'';
+$_=decode_base64($_);
+say;
+$rh=decode_json($_);
+if($rh->{"add"} eq ""){say "格式无效"; exit;}
+$_='// https://www.v2ray.com/chapter_00/start.html
+{
+  "inbounds": [{
+    "port": 1080,  // 本机 SOCKS 代理端口
+    "listen": "127.0.0.1",
+    "protocol": "socks",
+    "settings": {
+      "udp": true
+    }
+  }],
+  "outbounds": [{
+    "protocol": "vmess",
+    "settings": {
+      "vnext": [{
+        "address": "xxxadd", // 服务器地址
+        "port": xxxport,  // 服务器端口
+		// ID 和 AlterID
+        "users": [{ "id": "xxxid", "alterId": xxxaid }]
+      }]
+    }
+  },{
+    "protocol": "freedom",
+    "tag": "direct",
+    "settings": {}
+  }],
+  "routing": {
+    "domainStrategy": "IPOnDemand",
+    "rules": [{
+      "type": "field",
+      "ip": ["geoip:private"],
+      "outboundTag": "direct"
+    }]
+  }
+}
+';
+s/xxxadd/$rh->{add}/; s/xxxport/$rh->{port}/;
+s/xxxid/$rh->{id}/; s/xxxaid/$rh->{aid}/;
+open OUT,">$ENV{HOME}/vv-$rh->{ps}.json";
+print OUT $_; close OUT;
