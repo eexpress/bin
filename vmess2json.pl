1f0c2c70b0b756f3ebb806d2ac874dd97ed1e0ce
eexpress <eexpress@163.com>
2019-10-20 17:15:36 +0800

    clip.ss2json.pl

--- vmess2json.pl
+++ vmess2json.pl
@@ -10,39 +10,39 @@ $_=decode_base64($_);
 say; say "==================";
 $rh=decode_json($_);
 if($rh->{"add"} eq ""){say "格式无效"; exit;}
+
+# 输出成v2ray格式的json文件
 $_='// https://www.v2ray.com/chapter_00/start.html
 {
-  "inbounds": [{
-    "port": 1080,  // 本机 SOCKS 代理端口
-    "listen": "127.0.0.1",
-    "protocol": "socks",
-    "settings": {
-      "udp": true
-    }
-  }],
-  "outbounds": [{
-    "protocol": "vmess",
-    "settings": {
-      "vnext": [{
-        "address": "xxxadd", // 服务器地址
-        "port": xxxport,  // 服务器端口
-		// ID 和 AlterID
-        "users": [{ "id": "xxxid", "alterId": xxxaid }]
-      }]
-    }
-  },{
-    "protocol": "freedom",
-    "tag": "direct",
-    "settings": {}
-  }],
-  "routing": {
-    "domainStrategy": "IPOnDemand",
-    "rules": [{
-      "type": "field",
-      "ip": ["geoip:private"],
-      "outboundTag": "direct"
-    }]
-  }
+	"inbounds": [{
+		"port": 1080,  // 本机 SOCKS 代理端口
+		"listen": "127.0.0.1",
+		"protocol": "socks",
+		"settings": { "udp": true }
+	}],
+	"outbounds": [{
+		"protocol": "vmess",	//协议
+		"settings": {
+		"vnext": [{
+			"address": "xxxadd", // 服务器地址
+			"port": xxxport,  // 服务器端口
+			// ID 和 AlterID
+			"users": [{ "id": "xxxid", "alterId": xxxaid }]
+		}]
+	}
+	},{
+	"protocol": "freedom",
+	"tag": "direct",
+	"settings": {}
+	}],
+	"routing": {
+	"domainStrategy": "IPOnDemand",
+	"rules": [{
+		"type": "field",
+		"ip": ["geoip:private"],
+		"outboundTag": "direct"
+	}]
+	}
 }
 ';
 s/xxxadd/$rh->{add}/; s/xxxport/$rh->{port}/;
