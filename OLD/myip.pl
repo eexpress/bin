8c8d0ae28492f0b43ccf123729eb60d9d57ebe39
eexpress <eexpress@163.com>
2019-10-23 20:18:58 +0800

    myip.pl

--- OLD/myip.pl
+++ OLD/myip.pl
@@ -1,6 +1,18 @@
 #!/usr/bin/perl
 
+use 5.010;
+
+$url="http://icanhazip.com/";
+#$url="http://ifconfig.me/ip";
+
+#-------------------
+print "LWP::Simple:\t\t";
 use LWP::Simple;
-$content = get("http://ifconfig.me/ip");
-die "Couldn't get it!" unless defined $content;
-print $content;
+$content = get($url);
+if(defined $content){say $content;}else{say "failure.";}
+#-------------------
+print "LWP::UserAgent:\t\t";
+use LWP::UserAgent;
+$ua = LWP::UserAgent->new( agent => 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0', timeout => 20,);
+$response = $ua->get($url);
+if ($response->is_success){say $response->decoded_content;}else{say "failure.";}
