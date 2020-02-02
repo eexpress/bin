2fff176243269a54dabe3ae54608b03023f07caf
eexpress <eexpress@163.com>
2020-02-02 22:07:51 +0800

    dotchar增加边框切换

--- bitmap点阵字模/dotchar.pl
+++ bitmap点阵字模/dotchar.pl
@@ -1,13 +1,12 @@
 #!/usr/bin/perl
 
 use 5.010;
-use utf8;
+use utf8::all;	# abs_path居然受影响，变乱码
 use Encode;
 use Getopt::Std;
-use File::Basename;
 no warnings 'experimental::smartmatch';
 #-------------------------------------
-getopts('hb');
+getopts('hbx');
 
 $f="HZK12"; $size=24;	# 12点阵
 $vmode=1;	# 缺省垂直输出
@@ -20,15 +19,20 @@ $in=$_//"点阵字模";
 say "-------------------------------------";
 say "使用 -h 设置水平输出，缺省垂直输出。";
 say "使用 -b 指定16点阵，缺省12点阵输出。";
+say "使用 -x 切换边框显示，缺省底色显示。";
 say "当前使用的点阵字库：$f";
 say "-------------------------------------";
 #-------------------------------------
-open IN,dirname($0)."/$f" or die $!;
+use File::Basename;
+use Cwd "abs_path";
+chdir dirname(abs_path($0));
+
+open IN,"$f" or die $!;
 binmode(IN);
 $index=0;
 #-------------------------------------
 foreach $str (split //, $in){
-	$str=encode( "gb2312",$str);	# utf8字符转成区位码
+	$str=encode("gb2312",$str);	# utf8字符转成区位码
 	($Q, $W)=unpack("C*", $str);
 	$pos=(($Q-0xA0-1)*94+($W-0xA0-1))*$size;	# 偏移量
 	seek(IN, $pos, 0);
@@ -57,7 +61,8 @@ close IN;
 $separate="x"x(length($out[0]));	# 加边框
 unshift(@out, $separate);
 push(@out, $separate);
-for(@out){ s/(.*)/x$1x/; s/x/⚫/g; s/0/⚫/g; s/1/⚪/g; say; }
+$rep="⚫"; if($opt_x){$rep=" ";}
+for(@out){ s/(.*)/x$1x/; s/x/$rep/g; s/0/⚫/g; s/1/⚪/g; say; }
 #-------------------------------------
 #特殊的Unicode字符，找不到对应的字体。
 #⭕ cat t|convert -pointsize 24 -font Fira-Mono-Regular label:@- t.png
