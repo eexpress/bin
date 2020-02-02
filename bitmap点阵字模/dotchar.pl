aea943bc47fdad8ffc9ae5fd72e54ebdadb6b998
eexpress <eexpress@163.com>
2020-02-02 21:29:31 +0800

    dotchar指定同目录字库路径

--- bitmap点阵字模/dotchar.pl
+++ bitmap点阵字模/dotchar.pl
@@ -1,18 +1,19 @@
 #!/usr/bin/perl
 
 use 5.010;
-use utf8::all;
+use utf8;
 use Encode;
 use Getopt::Std;
+use File::Basename;
 no warnings 'experimental::smartmatch';
 #-------------------------------------
 getopts('hb');
 
 $f="HZK12"; $size=24;	# 12点阵
-$vmode=1;	# 缺省垂直
+$vmode=1;	# 缺省垂直输出
 
-if($opt_h){$vmode=0;}
-if($opt_b){$f="HZK16"; $size=32;}
+if($opt_h){$vmode=0;}	# 水平输出
+if($opt_b){$f="HZK16"; $size=32;}	# 16点阵
 
 $_=shift;
 $in=$_//"点阵字模";
@@ -22,12 +23,11 @@ say "使用 -b 指定16点阵，缺省12点阵输出。";
 say "当前使用的点阵字库：$f";
 say "-------------------------------------";
 #-------------------------------------
-open IN,"$f" or die $!;
+open IN,dirname($0)."/$f" or die $!;
 binmode(IN);
 $index=0;
 #-------------------------------------
-foreach $str (split //, $in){	# utf8::all，下句不需要decode了。
-#	$str=encode( "gb2312",decode("utf8",$str));	# utf8字符转成区位码
+foreach $str (split //, $in){
 	$str=encode( "gb2312",$str);	# utf8字符转成区位码
 	($Q, $W)=unpack("C*", $str);
 	$pos=(($Q-0xA0-1)*94+($W-0xA0-1))*$size;	# 偏移量
@@ -35,14 +35,14 @@ foreach $str (split //, $in){	# utf8::all，下句不需要decode了。
 	read(IN,$buf,$size);
 #-------------------------------------
 	if(!$vmode){$index=0;}	# 水平输出，字符串追加。
+	# 垂直，除开第一个字，字之前加整行间隔
+	if($vmode && $index>0){$out[$index]="x"x($size/2); $index++;}
+# unpack 说明
 #	S				An unsigned short value.
 #	> sSiIlLqQ		Force big-endian byte-order on the type.
-	# 垂直，除开第一个字，字之前加间隔
-	if($vmode && $index>0){$out[$index]="x"x($size/2); $index++;}
 	foreach $ch (unpack("S>*",$buf)){
 		$_=sprintf("%016b",$ch);
 		$omit="."x(16-$size/2); s/$omit$//;	# 尾部4位丢弃
-#		if($frame){$_="x".$_."x";}	# 逐字左右加边
 		if($vmode){
 			$out[$index]=$_;
 		}else{
