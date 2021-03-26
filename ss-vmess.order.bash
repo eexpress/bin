9278401e96f5262158c2ab20ab199eb02df87269
eexpress <eexpress@163.com>
2021-03-27 01:22:36 +0800

    2021-03-27 01:22:36

--- ss-vmess.order.bash
+++ ss-vmess.order.bash
@@ -8,12 +8,12 @@
 #~ $orderurl = "https://iwxf.netlify.app"
 orderurl="https://raw.githubusercontent.com/freefq/free/master/v2"
 file="/tmp/ss-vmess.order.html"
-wget $orderurl -O $file
+wget $orderurl -O - | base64 -d >$file
 #~ -----------------------------------------------
 #~ 保存 json 文件到 “~/app/v2ray的配置” 目录
-cat $file | base64 -d | ss-vmess-QRcode-json.pl -p
+cat $file | ss-vmess-QRcode-json.pl -p
 
 #~ -----------------------------------------------
 # 执行前，手机打开 kde connect
-cat $file | base64 -d | xclip -i -selection clipboard
+cat $file | xclip -i -selection clipboard
 # 执行后，v2rayNG 中“从剪贴板导入”
