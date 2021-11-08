b8a0265dbdf88421328895416820e3dcf027276d
eexpress <eexpress@163.com>
2021-11-09 00:04:02 +0800

    test push of gitg

--- ocr-screen.bash
+++ ocr-screen.bash
@@ -4,5 +4,5 @@ png="/tmp/ocr.png"
 txt="/tmp/ocr"
 
 gnome-screenshot -a -f $png
-tesseract $png $txt -l chi_sim
+tesseract $png $txt -l chi_sim #中文
 zenity --text-info --title="文字识别结果" --filename=$txt".txt"
