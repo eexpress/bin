37a2576081ef79a7cc7b68988e6c971adf40a02c
eexpress <eexpress@163.com>
2022-02-13 16:27:54 +0800

    puzzle.pl

--- puzzle.pl
+++ puzzle.pl
@@ -1,25 +1,38 @@
 #!/usr/bin/perl
 
-use 5.010;
+use 5.10.0;
+#~ 可放入nautilus脚本目录，`~/.local/share/nautilus/scripts`。
+#~ nautilus会直接给此脚本加参数，但是参数无路径。只能解析 `$ENV{}`。
 
-die "需要2个及以上图片，才能拼接。" if scalar @ARGV < 2;
-$f = join(' ',map {qq/"$_"/} @ARGV);
+sub end{
+	`notify-send "要2个及以上图片，才能拼接。"`;
+	die "需要2个及以上图片，才能拼接。" ;
+}
+
+@_ = split '\n', $ENV{'NAUTILUS_SCRIPT_SELECTED_FILE_PATHS'};
+#~ @_ //= @ARGV;	# 可惜 Defined-or 操作不能用于数组。
+if (scalar @_ > 1){
+		$f = join(' ',map {qq/"$_"/} @_);
+} elsif (scalar @ARGV > 1){
+	$f = join(' ',map {qq/"$_"/} @ARGV);
+}else{ end; }
+
+#~ `notify-send "debug: $f"`;
 @r = `identify -format "%w %h %d/%f\n" $f`;
 map {s/(\d{2,})\ /sprintf("%05d\ ", $1)/eg} @r;
 @r = sort @r;
 say @r;
-$i = scalar @r;
-die "需要2个及以上图片，才能拼接。" if $i < 2;
+$filenum = scalar @r;
+end if $filenum < 2;
 
-$x = int(sqrt($i));
-$y = $x+1;
+my $x = int(sqrt($filenum));
+my $y = $x+1;
 #~ 列数 = 文件数开平方，取整；+0的平方，+1的平方，看离哪个平方最近。
-if($y**2-$i > $i-$x**2){$column = $x;}else{$column = $y;}
+if($y**2-$filenum > $filenum-$x**2){$column = $x;}else{$column = $y;}
 #~ 最宽不参与排序的个数 = $#取模列数
-$dropout = $i % $column;
-say "文件数 = $i，列数 = $column，最宽不参与排序的个数 = $dropout";
+$dropout = $filenum % $column;
+say "文件数 = $filenum，列数 = $column，最宽不参与排序的个数 = $dropout";
 
-$suffix_w = 0;
 while (glob("/tmp/w-*.png")) {
 	next if -d; unlink $_ or ++$errors, warn("Can't remove $_: $!");
 }
@@ -29,10 +42,13 @@ if($dropout){
 	while ($dropout>0){push @h, pop @r; $dropout--;}
 	horizon_join(1);
 }
+
+$suffix_w = 0;
 $pop = 1;
 while (@r){
 	@h = ();
-	for($i=0; $i<$column; $i++){
+	#~ for($i=0; $i<$column; $i++){
+	for(1 .. $column){
 		if($pop){
 			push @h, pop @r;
 			$pop = 0;
@@ -61,7 +77,7 @@ sub horizon_join(){
 	}
 	say @h;
 	$min = 10000;
-	for($i = 0; $i <= $#h; $i++){
+	for my $i (0 .. $#h){	# $#h 是最大标量，比数组长度少1。
 		@r0 = split ' ', @h[$i];
 		$h1 = @r0[1];
 		@h[$i] = @r0[2];
@@ -92,6 +108,7 @@ sub horizon_join(){
 	say $cmd;
 	`$cmd`;
 	if($is_horizon){$suffix_w++;}
+	else{`eog /tmp/$o-$suffix_w.png`;}
 
 }
 
