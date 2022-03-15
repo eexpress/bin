15cd72aa8a7b7d90202621538f18793c24fc3d38
eexpress <eexpress@163.com>
2022-03-15 15:07:27 +0800

    hugo.ci.bash

---
+++ hugo.ci.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f config.toml ]; then
+	echo "Not in Hugo directory"
+	exit
+fi
+hugo
+cd public
+
+msg=${*:-`date '+%F %T'`}
+git ci -a -m "$msg"; git push
+# ci -a 自动先把工作区的文件add/rm到了暂存区，然后commit到本地仓库。
+# ci -m 是提交时，附带注释字符串。
+
