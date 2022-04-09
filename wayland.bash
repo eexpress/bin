05e85bf2500e62b08dde9652d181ef73abf1914f
eexpress <eexpress@163.com>
2022-04-09 11:33:18 +0800

    wayland.bash

--- wayland.bash
+++ wayland.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # wayland的软件，很多都开在桌面窗口的外面了。需要设置WAYLAND_DISPLA。
-#~ env MUTTER_DEBUG_DUMMY_MODE_SPECS=1600x1000 WAYLAND_DISPLAY=wayland-test-0 dbus-run-session gnome-shell --nested --wayland-display=wayland-test-0
+env MUTTER_DEBUG_DUMMY_MODE_SPECS=1400x900 WAYLAND_DISPLAY=wayland-test-0 dbus-run-session gnome-shell --nested --wayland-display=wayland-test-0
 
-env MUTTER_DEBUG_DUMMY_MODE_SPECS=1400x900 dbus-run-session gnome-shell --nested
+#~ env MUTTER_DEBUG_DUMMY_MODE_SPECS=1400x900 dbus-run-session gnome-shell --nested
 
 #~ dbus-run-session gnome-shell --nested
