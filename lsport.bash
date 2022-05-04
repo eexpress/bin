fb884835b82d7144ceca2eec0011f0773442a30a
eexpress <eexpress@163.com>
2022-05-04 18:28:36 +0800

    lsport.bash

--- lsport.bash
+++ lsport.bash
@@ -5,10 +5,7 @@
 
 # 列出本机的端口
 echo -e " 端口\t进程\t服务"
-#sudo netstat -tpan|grep 0.0.0.0|sed 's/^[^:]*://'|sed 's/\ \+.*LISTEN\ \+/\t/g'|sed 's/\//\t/'|sort -g
 
-#~ netstat -tpan 2>/dev/null|perl -lane 'next if @F[0] !~ /tcp[6]$/ | @F[5] !~ /LISTEN/; @F[3]=~s/.*://; @F[6]=~s/\//\t/; print "\e[1;34m @F[3]\e[0m\t@F[6]";'
-
-# --tcp --listening --processes --numeric
-sudo ss -tlpn| perl -lane 'next if ! /users/; @F[3]=~s/.*://; /"(.*)",pid=(.*),/; print "\e[1;34m @F[3]\e[0m\t$2\t$1"'
+# --tcp --listening --processes --numeric --ipv4
+sudo ss -tlpn4| perl -lane 'next if ! /users/; @F[3]=~s/.*://; /"(.*)",pid=(.*),/; print "\e[1;34m @F[3]\e[0m\t$2\t$1"'
 
