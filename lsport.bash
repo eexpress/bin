b7e9351d124827dc7fba43e50a4f251aab75e075
eexpress <eexpress@163.com>
2022-05-04 18:39:46 +0800

    lsport.bash

--- lsport.bash
+++ lsport.bash
@@ -7,5 +7,10 @@
 echo -e " 端口\t进程\t服务"
 
 # --tcp --listening --processes --numeric --ipv4
-sudo ss -tlpn4| perl -lane 'next if ! /users/; @F[3]=~s/.*://; /"(.*)",pid=(.*),/; print "\e[1;34m @F[3]\e[0m\t$2\t$1"'
-
+sudo ss -tlpn4| perl -lane '
+next if ! /users/;
+@F[3]=~s/.*://; /"(.*)",pid=(.*),/;
+print "\e[1;34m @F[3]\e[0m\t$2\t$1"
+'
+#~ LISTEN  0  128  127.0.0.1:631   0.0.0.0:*  users:(("cupsd",pid=591,fd=7))
+#~ 第3列删除冒号前内容，获取引号中内容，获取pid=后的内容
