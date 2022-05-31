27573c8d5aea20c61d0c1a8e028e0eaab2389c58
eexpress <eexpress@163.com>
2022-05-31 18:26:59 +0800

    rpm/deb打包脚本

--- create-rpm.pl
+++ create-rpm.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
-# fedora 下需要 `sudo dnf install dpkg perl-File-MimeInfo`
-# ubuntu 下需要 `sudo apt install libfile-mimeinfo-perl`
+# fedora 下需要 `sudo dnf install rpmdevtools perl-File-MimeInfo`
+# ubuntu 下需要 `sudo apt install rpm libfile-mimeinfo-perl`
 use 5.10.0;
 use File::MimeInfo;	#需要安装 perl-File-MimeInfo(fedora) / libfile-mimeinfo-perl(ubuntu)
 use File::Copy qw(copy cp);
@@ -12,48 +12,45 @@ if(scalar @ARGV == 0){
 	die '----------------------------
 Input binary / picture / [desktop] file as parameter,
 Version parameter can be "v0.2" or "v1".
-Create deb structrue directory for build deb pacake.
-Auto create "control" file, auto fill some fields.
+Create rpm structrue directory for build rpm pacake.
+Auto create ".spec" file, auto fill some fields.
 If no desktop file, auto create and fill one.
 ----------------------------';
 }
 #~ ---------------------------------------------
-my %deb = ('Priority'=> 'optional|extra (<===== select only one)', 'Section'=> 'utils|web|net|misc|x11 (<===== select only one)',
- 'Depends'=>'<===== xxxx,xxxx','Description'=>"<===== xxxx\n  <====xxxxxxxxxxxxxxx", 'Version'=>"1.0");
-#~ Priority	软件对于系统的重要程度	required, standard, optional, extra等；
-#~ Section	软件类别	utils, net, mail, text, x11
+my %rpm = ('BuildArch'=>'noarch', 'Release'=>'1%{?dist}', 'License'=>'GPLv3+', 'Source0'=>'%{name}-%{version}.tar.xz',
+ 'Requires'=>'<===== xxxx,xxxx','Summary'=>"<===== xxxxxxxxxxxxxxx", 'Version'=>"1.0");
+#~ Group	软件类别	utils, net, mail, text, x11
 my @bin, @img, @desktop;
 
-my $arch = `dpkg --print-architecture`;	# 自动获取系统架构。
+my $arch = `arch`;	# 自动获取系统架构。
 chomp $arch;
-$deb{'Architecture'} = $arch;
-my $user = $ENV{'USER'};	# 自动填充维护者信息。
-$deb{'Maintainer'} = $user." <$user\@gmail.com>";
+$rpm{'BuildArch'} = $arch;
+#~ my $user = $ENV{'USER'};	# 自动填充维护者信息。
+#~ $rpm{'Maintainer'} = $user." <$user\@gmail.com>";
 my $git = `git remote -v`;	# 自动填写主页，需在 git 目录下。
 $git =~ m'\s(.*@.*:.*/.*)\s*\(fetch\)';
 my $repo = $1;
 if($repo){
 	$_ = $repo;
 	s':'/'; s'^git@'https://';
-	$deb{'Homepage'} = $_;
+	$rpm{'URL'} = $_;
 }
 #~ ---------------------------------------------
 my $version = "-1.0";
 for (@ARGV){
 	if(/^v(\d[\.\d]{0,})/){	# 接受一个版本参数，v0.2 或者 v1 这样的。
-		$deb{'Version'} = $1;
+		$rpm{'Version'} = $1;
 		$version = "-$1";	# 版本号会附加到路径。
 		next;
 	}
 	if(! -f) {next;}	# 其他参数只能是文件。
-	#~ my @info = stat($_);	# 获取文件大小。
-	#~ $deb{'Installed-Size'} += $info[7];	# 累加到安装尺寸。
 	my $file = $_;
 	given (mimetype($file)) {	# 按文件类型放到对应目录
 		when ("application/octet-stream") {	# 二进制文件。
 			if(! -x $file){say "$file no excutable."; next;} # 可执行，才放入 bin 目录。
 			push @bin, $file;
-			if(! $deb{'Package'}) {$deb{'Package'} = $file;} #第一个可执行文件当作包名。
+			if(! $rpm{'Name'}) {$rpm{'Name'} = $file;} #第一个可执行文件当作包名。
 		}
 		when (m'^image/') {		# 图片文件。
 			push @img, $file;
@@ -65,25 +62,29 @@ for (@ARGV){
 			say "$file symlink not allow.";
 		}
 		default {	# 无效类型。
-			say "$file mimetype ($_) not use.";
+			if(! -x $file){say "$file mimetype ($_) not use."; next;}
+			push @bin, $file;		# python 执行文件判断不出。
 		}
 	}
 }
-#~ $deb{'Installed-Size'} = int($deb{'Installed-Size'}/1024)." KB";	# 计算安装尺寸。
+#~ $rpm{'Installed-Size'} = int($rpm{'Installed-Size'}/1024)." KB";	# 计算安装尺寸。
 #~ ---------------------------------------------
 if ($#bin < 0) {die "Need at last one binary excute file.";}
 if ($#img < 0) {die "Need at last one image file.";}
-my $path = "/tmp/$deb{'Package'}$version";
-remove_tree($path);
-make_path("$path/DEBIAN", "$path/usr/bin", "$path/usr/share/applications", "$path/usr/share/pixmaps");
-for(@bin){cp $_, "$path/usr/bin/";}
-for(@desktop){cp $_, "$path/usr/share/applications/";}
-for(@img){cp $_, "$path/usr/share/pixmaps/";}
+my $path0 = "$ENV{HOME}/rpmbuild";
+remove_tree($path0);
+`rpmdev-setuptree`;	# 遵照 ~/.rpmmacros 设置，产生 rpmbuild 目录。
+$path = "$path0/SOURCES";
+make_path("$path/usr/bin", "$path/usr/share/applications", "$path/usr/share/pixmaps");
+my $filelist = '';
+for(@bin){cp $_, "$path/usr/bin/"; $filelist .= "%{_bindir}/$_\n";}
+for(@desktop){cp $_, "$path/usr/share/applications/"; $filelist .= "%{_datadir}/applications/$_\n";}
+for(@img){cp $_, "$path/usr/share/pixmaps/"; $filelist .= "%{_datadir}/pixmaps/$_\n";}
 #~ ---------------------------------------------
 my $desktop_msg = "";
 if($#desktop < 0){	# 如果没有桌面文件，自动新建一个。
 	my $dpath = "usr/share/applications/$bin[0].desktop";
-	$desktop_msg = " and $bin[0].desktop";
+	$desktop_msg = " and SOURCES/.../$bin[0].desktop";
 	my $desktoptxt = "[Desktop Entry]
 Type=Application
 Terminal=false
@@ -93,38 +94,47 @@ Exec=$bin[0]";
 	open FC, ">", "$path/$dpath";
 	say FC $desktoptxt;
 	close FC;
+	$filelist .= "%{_datadir}/applications/$bin[0].desktop\n";
 	system("xdg-open $path/$dpath");
 }
 #~ ---------------------------------------------
-save_control();
-$_ = `du -sh $path`; s/\s+.*$//; chomp;	# du的第一个结果数据。
-$deb{'Installed-Size'} = $_;
-`tar -Jcf tmp.xz $path 2>/dev/null`;
-$_ = `du -sh tmp.xz`; s/\s+.*$//; chomp;	# du的第一个结果数据。
-$deb{'Download-Size'} = $_;
-save_control();
-#~ ---------------------------------------------
-system("xdg-open $path/DEBIAN/control");
+my $specfile = "$path0/SPECS/$rpm{'Name'}.spec";
+save_spec();
+system("xdg-open $specfile");
 #~ ---------------------------------------------
 say "==============================================";
-my $tree = `tree $path`;
+my $tree = `tree $path0`;
 chomp $tree;
 say $tree;
 say "==============================================";
-say "Please confirm DEBIAN/control$desktop_msg. ";
+say "Please confirm SPECS/$rpm{'Name'}.spec$desktop_msg. ";
 say "==============================================";
-say "After comfirm, you can excute `sudo dpkg -b $path` will create deb file:\n==>\t$path.deb";
+say "After comfirm, you can excute `rpmbuild -bb ~/rpmbuild/SPECS/$rpm{'Name'}.spec ` will create rpm file:\n==>\t~/rpmbuild/RPMS/$arch/$rpm{'Name'}-$rpm{'Version'}-$rpm{'Release'}.rpm";
 
-sub save_control{
+sub save_spec{
 	my $control = '';	# 新建 control 文件。
-	my @order = ('Package', 'Version', 'Priority', 'Section', 'Maintainer', 'Installed-Size',
-	'Download-Size', 'Homepage', 'Architecture', 'Depends', 'Description');
+	my @order = ('Name', 'Version',  'Release', 'Summary', 'BuildArch', 'License', 'URL', 'Source0', 'Requires', );
 	for(@order){
-		$field = sprintf("%-14s", $_);
-		$control .= "$field : $deb{$_}\n";
+		$field = sprintf("%-16s", "$_:");
+		$control .= "$field $rpm{$_}\n";
 	}
 	chomp $control;
-	open FC, ">", "$path/DEBIAN/control";
+	my $other = "
+%description
+<===========xxxxxxxxxxxxx
+
+%define _binaries_in_noarch_packages_terminate_build   0
+
+%install
+cd %{_sourcedir}
+cp -ar * %{buildroot}/
+
+%files
+$filelist
+
+	";
+	open FC, ">", $specfile;
 	say FC $control;
+	say FC $other;
 	close FC;
 }
