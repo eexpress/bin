791f4507a64a264ca22f56d45c47ae34fe24ead4
eexpress <eexpress@163.com>
2022-08-16 00:13:58 +0800

    增加说明

--- OLD/ansi-color-256.pl
+++ OLD/ansi-color-256.pl
@@ -5,7 +5,8 @@
 $bold="\e[1m"; $boldrev="\e[1;7m"; $boldund="\e[1;4m"; $end="\e[0m";
 
 print "ESCAPE:\t\\e  \\x1b  \\x33\nFormat:\t1 Bold 2 Dim 4 Underline 5 Blink 7 Reverse(invert fg and bg) 8 Hidden(password)\n\t${boldund}2x${end} Reset above attributes; ${boldund}0${end} Reset all attributes, \\e[0m\n\n";
-print "16 Colors:\t\\e[${boldrev}3/4/9/10${end}[0-7]m\t${bold}Fg${end}=3/9\t${boldrev}Bg${end}=4/10\n\n";
+print "eg: \\e[31;44;1;5m  red fg + blue bg + blod + blink\n\n";
+print "16 Colors:\t\\e[${boldrev}3/4/9/10${end}[0-7]m\t${bold}Fg${end}=3/9\t${boldrev}Bg${end}=4/10\t[0-7]=16 colors\n\n";
 for ($color = 0; $color < 16; $color++) {
 	$tc=(7-$color%8);
 	$index=$color%8;
@@ -15,7 +16,7 @@ for ($color = 0; $color < 16; $color++) {
 }
 print "$end\n\n";
 
-print "256 Colors:\t\\e[${boldrev}3/4${end}8;5;[0-255]m\t${bold}Fg${end}=3\t${boldrev}Bg${end}=4\n\n";
+print "256 Colors:\t\\e[${boldrev}3/4${end}8;5;[0-255]m\t${bold}Fg${end}=3\t${boldrev}Bg${end}=4\t8=8 bits 256 colors\n\n";
 
 for $index (0,3){
 for ($green = 0; $green < 6; $green++) {
