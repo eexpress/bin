414ef3cb7bdbb464ef84754841006098b86d7433
eexpress <eexpress@163.com>
2023-11-07 20:36:26 +0800

    dir-xml-gresource.pl

--- dir-xml-gresource.pl
+++ dir-xml-gresource.pl
@@ -1,6 +1,6 @@
 #!/bin/perl
 
-#~ parameter: dir
+#~ 将多个路径下的文件，建立res.xml列表，并打包成res.gresource文件。
 #~ ⭕ dir-xml-gresource.pl img icon
 #~ =====> res.xml
 #~ =====> res.gresource
@@ -46,3 +46,16 @@ say "=====> ".$f;
 $list = `gresource list $f`;
 say $list;
 
+#~ 从gresource文件释放全部资源
+#~ ⭕ pi glib2-devel
+#~ ⭕ gresource list res.gresource
+#~ /img/r1.svg
+#~ /img/r2.svg
+#~ /img/r3.svg
+#~ /img/r4.svg
+#~ /img/r5.svg
+#~ /img/r6.svg
+#~ /img/r7.svg
+#~ /img/r8.svg
+#~ /img/r9.svg
+#~ ⭕ gresource list res.gresource | while read i; do gresource extract res.gresource $i>./$i; done
