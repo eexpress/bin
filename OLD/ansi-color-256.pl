013e86ff73f8f6b33da25823a40be1a38a422e10
eexpress <eexpress@163.com>
2023-12-03 18:43:28 +0800

    ansi-color-256.pl

--- OLD/ansi-color-256.pl
+++ OLD/ansi-color-256.pl
@@ -2,11 +2,14 @@
 # Author: Todd Larason <jtl@molehill.org>
 # modify: eexpress@163.com
 # display the colors
-$bold="\e[1m"; $boldrev="\e[1;7m"; $boldund="\e[1;4m"; $end="\e[0m";
+$bold="\e[1m"; $boldrev="\e[1;7m"; $boldund="\e[1;4m"; $end="\e[0m"; $dimrev="\e[2;7m";
 
-print "ESCAPE:\t\\e  \\x1b  \\x33\nFormat:\t1 Bold 2 Dim 4 Underline 5 Blink 7 Reverse(invert fg and bg) 8 Hidden(password)\n\t${boldund}2x${end} Reset above attributes; ${boldund}0${end} Reset all attributes, \\e[0m\n\n";
-print "eg: \\e[31;44;1;5m  red fg + blue bg + blod + blink\n\n";
-print "16 Colors:\t\\e[${boldrev}3/4/9/10${end}[0-7]m\t${bold}Fg${end}=3/9\t${boldrev}Bg${end}=4/10\t[0-7]=16 colors\n\n";
+print "ESCAPE:\t\\e  \\x1b  \\033\nFormat:\t1 Bold 2 Dim 4 Underline 5 Blink 7 Reverse 8 Hidden(password)\n\t${boldund}2x${end} Reset above attributes; ${boldund}0${end} Reset all attributes, \\e[0m\n\n";
+print "eg: \\e[31;44;1;5m  red fg + blue bg + blod + blink\n";
+#~ print "16 Colors:\t\\e[${boldrev}3/4/9/10${end}[0-7]m\t${bold}Fg${end}=3/9\t${boldrev}Bg${end}=4/10\t[0-7]=16 colors\n\n";
+print "-"x80;
+print "\n16 Colors:\t${dimrev}\\e[${end} ${boldrev}3/4/9/10${end} ${boldrev}0-7${end} ${dimrev}m${end}\n\n";
+print "1st group. ${bold}Fg${end}=3, ${boldrev}Bg${end}=4\t\t2nd group. ${bold}Fg${end}=9, ${boldrev}Bg${end}=10 \n";
 for ($color = 0; $color < 16; $color++) {
 	$tc=(7-$color%8);
 	$index=$color%8;
@@ -14,9 +17,9 @@ for ($color = 0; $color < 16; $color++) {
 	printf "\e[38;5;${tc};48;5;${color}m $index ";
 #        printf "\e[38;5;${tc};48;5;${color}m %s",sprintf "%d-%d", ${color}, $index;
 }
-print "$end\n\n";
+print "$end\n".("-"x80)."\n";
 
-print "256 Colors:\t\\e[${boldrev}3/4${end}8;5;[0-255]m\t${bold}Fg${end}=3\t${boldrev}Bg${end}=4\t8=8 bits 256 colors\n\n";
+print "256 Colors:\t${dimrev}\\e[${end} ${boldrev}3/4${end} ${dimrev}8;5;${end} ${boldrev}0-255${end} ${dimrev}m${end}\t${bold}Fg${end}=3, ${boldrev}Bg${end}=4\t8=8 bits 256 colors\n\n";
 
 for $index (0,3){
 for ($green = 0; $green < 6; $green++) {
--- alidns.bash
+++ alidns.bash
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-dev=`ip address | grep -P '(?<!LOOPBACK),UP,' | awk '{print $2}' | sed 's/://g'`
-echo -e "\e[1;32mActive device:\e[0m" $dev
-sudo resolvectl dns $dev 223.5.5.5 223.6.6.6
-resolvectl | grep 'DNS Servers'
-#~ resolvectl | grep -A 5 $dev
-#~ resolvectl 不支持 --color=always
+#~ 获取发行版名称
+distro=`awk -F'=' '/^ID/{print $2}' /etc/os-release`
+if [ $distro == "fedora" ]; then
+	#~ 获取活动的网卡名
+	dev=`ip address | grep -P '(?<!LOOPBACK),UP,' | awk '{print $2}' | sed 's/://g'`
+	echo -e "\e[94mActive device:\e[1;32m" $dev"\e[0m"
+	#~ Fedora使用systemd-resolved作为默认的DNS解析器。
+	sudo resolvectl dns $dev 223.5.5.5 223.6.6.6
+	resolvectl | grep 'DNS Servers'
+	#~ resolvectl | grep -A 5 $dev
+	#~ resolvectl 不支持 --color=always
+fi
