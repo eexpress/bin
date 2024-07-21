2978f7aa7908e64ab6a7a3768da473d41d1a121b
eexpress <eexpress@163.com>
2024-07-21 16:39:27 +0800

    new

--- alidns.bash
+++ alidns.bash
@@ -4,11 +4,10 @@
 distro=`awk -F'=' '/^ID/{print $2}' /etc/os-release`
 if [ $distro == "fedora" ]; then
 	#~ 获取活动的网卡名
-	dev=`ip address | grep -P '(?<!LOOPBACK),UP,' | awk '{print $2}' | sed 's/://g'`
-	echo -e "\e[94mActive device:\e[1;32m" $dev"\e[0m"
+	#dev=`ip address | grep -P '(?<!LOOPBACK),UP,' | awk '{print $2}' | sed 's/://g'`
+	dev=`ip addr | perl -nE '/^\d:\s(.*):.*state UP/ && print $1;'`
+	#echo -e "\e[94mActive device:\e[1;32m" $dev"\e[0m"	#蓝色 + 绿色粗体
 	#~ Fedora使用systemd-resolved作为默认的DNS解析器。
 	sudo resolvectl dns $dev 223.5.5.5 223.6.6.6
-	resolvectl | grep 'DNS Servers'
-	#~ resolvectl | grep -A 5 $dev
-	#~ resolvectl 不支持 --color=always
+	resolvectl
 fi
