e7a29d375f84505df081f290514cd3c7e6954b5d
eexpress <eexpress@163.com>
2024-07-26 07:09:45 +0800

    统一fish/bash的提示符

--- prompt.pl
+++ prompt.pl
@@ -1,32 +1,29 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-
-use Encode qw/_utf8_on/;	# 输入参数强制指定
-use utf8;		# 本文内的 unicode 字符串。
-binmode STDOUT, ":utf8";		# Wide character in print
+use Encode qw/_utf8_on/;		# @ARGV
+use utf8;					# unicode in script
 
 sub colorize_dir_path {
-    my ($path) = @_;
-    my $colored_path = "\e[0m";
-    my @color_array = (22, 94, 18, 238, 52); # 绿 橙 蓝 灰 红
-    my $index = 0;
-    my $fgtext = "\e[38;5;255m";
+	my ($path) = @_;
+	my $colored_path = "\e[0m";
+	my @color_array = (22, 94, 18, 238, 52); # 绿 橙 蓝 灰 红
+	my $index = 0;
+	my $fgtext = "\e[38;5;255m";
 	my $color = "";
-    my @parts = split('/', $path);
-    foreach my $i (@parts) {
-        next if $i eq '';
-
-        $color = $color_array[$index];
-        $color = 22 if ($i eq "✔");	# 绿
-        $color = 52 if ($i eq "✘");	# 红
-        $index = ($index + 1) % scalar(@color_array);
+	my @parts = split('/', $path);
+	foreach my $i (@parts) {
+		next if $i eq '';
+		$color = $color_array[$index];
+		$color = 22 if ($i eq "✔");	# 绿
+		$color = 52 if ($i eq "✘");	# 红
+		$index = ($index + 1) % scalar(@color_array);
 		# 前景循环色+ ◢+ 背景循环色+ 前景灰色+ 一段路径文字
-        $colored_path .= "\e[38;5;${color}m◢\e[48;5;${color}m${fgtext} $i ";
-    }
+		$colored_path .= "\e[38;5;${color}m◢\e[48;5;${color}m${fgtext} $i ";
+	}
 	# 无颜色+ 前景最后循环色+ 箭头+ 无色
-    $colored_path .= "\e[0m\e[38;5;${color}m\x{E0B0}\e[0m";
-    return $colored_path;
+	$colored_path .= "\e[0m\e[38;5;${color}m\e[0m";
+	return $colored_path;
 }
 
 my $date = `date '+%a %T'`;
@@ -40,7 +37,7 @@ my $stdout = `git status --porcelain 2>/dev/null`;
 $path .= $stdout ? "/✘" : "/✔" if ($? >> 8 == 0);
 
 my $colored_path = colorize_dir_path($path);
-my $psch = "⭕";
-if ($ENV{'USER'} eq "root") {$psch = "🔴";}
+my $psch = ($ENV{'USER'} eq "root") ? "🔴" : "⭕";
 $colored_path .= "\n${psch} ";
+_utf8_on($colored_path);
 print $colored_path;
