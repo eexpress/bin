28caeef6f402c68cf2502c26d276ffb4f9c5e7ab
eexpress <eexpress@163.com>
2024-08-09 17:43:26 +0800

    wifi-adb-scrcpy.bash

--- wifi-adb-scrcpy.bash
+++ wifi-adb-scrcpy.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-PHONE=192.168.3.64
-adb connect ${PHONE}:$( nmap -sT ${PHONE} -p30000-49999 | awk -F/ '/tcp open/{print $1}' )
+# adb disconnect ALL-DEVICES
+# adb kill-server
+# adb start-server
 
-scrcpy
+PHONE=192.168.3.64
+PORT=$( nmap -sT ${PHONE} -p30000-49999 | awk -F/ '/tcp open/{print $1}' )
+# INFO=$( nmap -sT ${PHONE} -p30000-49999 | grep '/tcp' )
+# echo $INFO
+# PORT=$( echo $INFO | awk -F/ '/tcp open/{print $1}' )
+# PORT=$( nmap -sT ${PHONE} -p30000-49999 | awk -F/ '/tcp (open|filtered)/{print $1}' )
+if [ -z $PORT ]; then echo "no wifi port."; exit; fi
+# echo "====found device wifi port: "$PORT"===="
+
+adb connect ${PHONE}:$PORT && scrcpy
